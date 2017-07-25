ec3f89b0188d4d1f289507644d87b300be1aa8a7
Juraj Martinka <jumarko@gmail.com>
2017-07-25 10:27:45 +0200

    Add trivial Seesaw example.

--- src/clojure_repl_experiments/experiments.clj
+++ src/clojure_repl_experiments/experiments.clj
@@ -1,4 +1,14 @@
 (ns clojure-repl-experiments.experiments
   "Single namespace for all my REPL experiments.
-  This might be split up later if I find it useful.")
+  This might be split up later if I find it useful."
+  (:require [seesaw.core :as s]))
+
+;;; Seesaw tutorial: https://gist.github.com/1441520
+;;; check also https://github.com/daveray/seesaw
+(comment
+  (def f (s/frame :title "Get to know Seesaw"))
+  (-> f s/pack! s/show!)
+  )
+
+
 
