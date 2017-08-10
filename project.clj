2ae1621ebed7f44f0f20ec79df9c6bbe25956b72
Juraj Martinka <jumarko@gmail.com>
2017-08-10 15:04:31 +0200

    Add defrecord and protocol examples from Joy of Clojure.

--- project.clj
+++ project.clj
@@ -3,7 +3,9 @@
   :url "https://github.com/jumarko/clojure-repl-experiments"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha17"]]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
+                 [org.apache.pdfbox/pdfbox "2.0.7"]
+                 [net.java.dev.jna/jna "4.4.0"]]
   :main ^:skip-aot clojure-repl-experiments.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/clojure_repl_experiments/experiments.clj
+++ src/clojure_repl_experiments/experiments.clj
@@ -1,8 +1,7 @@
 (ns clojure-repl-experiments.experiments
   "Single namespace for all my REPL experiments.
   This might be split up later if I find it useful."
-  (:require [seesaw.core :as see]
-            [seesaw.core :as s]))
+  (:require [seesaw.core :as see]))
 
 ;;; Seesaw tutorial: https://gist.github.com/1441520
 ;;; check also https://github.com/daveray/seesaw
@@ -82,5 +81,86 @@ me-before
 (login :admin {:licensee "jumarko" :license-key "xyz"})
 
 (login :user {:licensee "jumarko" :license-key "xyz"})
+
+
+;; Petr Mensik's problem:
+;; How to access https://pdfbox.apache.org/docs/2.0.4/javadocs/org/apache/pdfbox/pdmodel/PDPageContentStream.AppendMode.html
+;; You have to import inner enum class
+(import org.apache.pdfbox.pdmodel.PDPageContentStream$AppendMode)
+org.apache.pdfbox.pdmodel.PDPageContentStream$AppendMode
+
+
+
+;;; Asked on clojurians slack - brave-and-true channel 3.8.2017
+(def filename "/Users/jumar/workspace/clojure/clojure-repl-experiments/resources/suspects.csv")
+(slurp filename)
+(def vamp-keys [:name :glitter-index])
+(defn str->int
+  [str]
+  (Integer. str))
+(def conversions {:name identity
+                  :glitter-index str->int})
+(defn convert
+  [vamp-key value]
+  ((get conversions vamp-key) value))
+(defn parse
+  "Convert a CSV into rows of columns"
+  [string]
+  (map #(clojure.string/split % #",")
+       (clojure.string/split string #"\r?\n")))
+(parse (slurp filename))
+(defn mapify
+  "Return a seq of maps like {:name \"Edward Cullen\" :glitter-index 10}"
+  [rows]
+  (map (fn [unmapped-row]
+         (reduce (fn [row-map [vamp-key value]]
+                   (assoc row-map vamp-key (convert vamp-key value)))
+                 {}
+                 (map vector vamp-keys unmapped-row)))
+       rows))
+(first (parse (slurp filename)))
+(first (mapify (parse (slurp filename)))) ;; NullPointer here
+(mapify (parse (slurp filename))) ;; and here
+
+
+;;;
+;;; defrecord and protocol experiments from Joy of Clojure
+;;;
+(defrecord TreeNode [v l r])
+(def my-tree (TreeNode. 0 nil nil))
+;; you can assoc new fields but the performance is as with regular map
+(assoc my-tree :color :red)
+;; you can dissoc fields but this will return normal map, not defrecord
+(type (dissoc my-tree :v))
+;; defrecord never equals to the map with same keys
+(= my-tree {:v 0 :l nil :r nil})
+
+
+;;; Clojure style mixins from Joy of Clojure - p. 211
+;;; notice that you have to always provide full implementation of protocol
+(defprotocol StringOps
+  (rev [s])
+  (upp [s]))
+
+(extend-type String
+  StringOps
+  (rev [s] (clojure.string/reverse s)))
+
+(rev "Works")
+
+;; now try to extend `upp` separately
+(extend-type String
+  StringOps
+  (upp [s] (clojure.string/upper-case s)))
+(upp "Works")
+;; but `rev` is lost now
+#_(rev "Works")
+;; but we still can define mixinx separately
+(def rev-mixin {:rev clojure.string/reverse})
+(def upp-mixin {:upp clojure.string/upper-case})
+(def fully-mixed (merge rev-mixin upp-mixin))
+(extend String StringOps fully-mixed)
+(-> "Works" upp rev)
+
 
 
