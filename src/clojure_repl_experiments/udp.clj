c8ec70b542520db6b3f2b89319f6eb9ab1dd712a
Juraj Martinka <jumarko@gmail.com>
2017-08-10 15:03:39 +0200

    Add Universal Design Pattern example from Joy of Clojure book.

---
+++ src/clojure_repl_experiments/udp.clj
@@ -0,0 +1,25 @@
+(ns clojure-repl-experiments.udp
+  "Universal Design Pattern as described in 'Joy of Clojure'."
+  (:require [clojure.core :exclude [get]]))
+
+;;; UDP describes 5 fundamental functions: beget, get, put, has?, forget.
+;;; We'll implement three most important one: beget, get, put
+
+(defn beget [this proto]
+  (assoc this ::prototype proto))
+
+(def child (beget {:child 1} {:parent 10}))
+
+(defn get [m k]
+  (when m
+    (if-let [[_ v] (find m k)]
+      v
+      (recur (::prototype m) k))))
+
+(get child :child)
+(get child :parent)
+(get child :grandparent)
+
+
+(def put assoc)
+(put child :likes :bananas)
