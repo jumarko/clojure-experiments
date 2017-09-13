871193cf0e1bf308aa9880f0a86260194dc10125
Juraj Martinka <jumarko@gmail.com>
2017-09-13 08:58:17 +0200

    Update clojure to 1.9.0-alpha20.
    Latest cider version from MELPA can work with this.

--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
   :url "https://github.com/jumarko/clojure-repl-experiments"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
-                 [org.clojure/clojure "1.9.0-alpha17" :classifier "sources"]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha20"]
+                 [org.clojure/clojure "1.9.0-alpha20" :classifier "sources"]
                  [org.apache.pdfbox/pdfbox "2.0.7"]
                  [net.java.dev.jna/jna "4.4.0"]
                  [org.flatland/useful "0.11.5"]]
