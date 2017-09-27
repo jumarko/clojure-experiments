930226561c3e2d7d15a3d9154180724c07b4acb2
Juraj Martinka <jumarko@gmail.com>
2017-09-27 10:30:57 +0200

    Exercises from Clojure Tutorials: OOP Lesson 3.

--- src/clojure_repl_experiments/oop.clj
+++ src/clojure_repl_experiments/oop.clj
@@ -1,5 +1,6 @@
 (ns clojure-repl-experiments.oop
-  "Object Oriented concepts - as applied. to Clojure.")
+  "Object Oriented concepts - as applied. to Clojure."
+  (:require [clojure.core.async :as async :refer [<!! >!!]]))
 
 ;;; Clojure Tutorials - OOP Lesson 1 - Basic of Object Oriented Programming
 ;;; https://www.youtube.com/watch?v=6tFSrnK7vmw&t=3s
@@ -93,3 +94,73 @@ Scoring
 
 (send-msg my-object :sum-sc [])
 (send-msg my-object :sum-scores [])
+
+
+
+;;; Clojure Tutorials - OOP Lesson 3 
+;;; https://www.youtube.com/watch?v=0RZ7DbEwkHo
+
+;; Back to the definition of Object Oriented Programming:
+;; http://www.dictionary.com/browse/object-oriented-programming
+"A Schematic paradigm for computer programming in which
+the linear concepts of procedures and tasks
+are replaced by the concepts of objects and messages.
+An object includes a package of data and a description
+of operations that can be performed on that data.
+A message specifies one of the operations, but unlike a procedure,
+does not describe how the operation should be carried out.
+C++ is an example of object oriented programming language."
+
+;; let's try to implement `print-obj`
+(defn print-obj [prefix]
+  (let [c (async/chan)]
+    (async/thread
+      (loop []
+        (when-some [[cmd data] (<!! c)]
+          (case cmd
+            :print (do (print prefix data)
+                       (recur))
+            (recur)))))
+    c))
+
+(let [o (print-obj "Printed -> ")]
+  (>!! o [:print 42])
+  (async/close! o))
+
+;; let's try it diferrently
+;; here we can see that we have local state `scores`
+;; and list of supported message in `case` body.
+(defn scoring-obj []
+  (let [c (async/chan)]
+    (async/thread
+      (loop [scores []]
+        (when-some [[cmd data] (<!! c)]
+          (case cmd
+            :add-score (recur (conj scores data))
+            :sum-scores (do (>!! data (apply + scores))
+                            (recur scores))
+            (do
+              (println "Bad Command : " cmd data)
+              (recur scores))))))
+    c))
+
+(let [o (scoring-obj)]
+  (>!! o [:add-score 42])
+  (>!! o [:add-score 1])
+  (let [reply-chan (async/chan)]
+    (>!! o [:sum-scores reply-chan])
+    (println "Summed: " (<!! reply-chan)))
+  (async/close! o))
+
+;; The previous implementation is pretty much the actor model!
+;;
+;; We as functional programmers often shy away from this because:
+;; - State is opaque / hidden in the object (`scores`)
+;; - In reality, you never know what's going to happen when you send a message.
+;; - There's no return value returned from "send message" operation
+;;
+;; We could easily pack our object and send it to the another machine.
+;;
+;; Actors combines the worst aspects of OOP (mutable state)
+;; with asynchronous programming which makes them even worse!
+
