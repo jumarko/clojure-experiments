f94c9de60f6cd9efc7258f7a85fca03afb1e0517
Juraj Martinka <jumarko@gmail.com>
2017-10-10 10:41:03 +0200

    Short example for clj-slack usage.

---
+++ src/clojure_repl_experiments/slack.clj
@@ -0,0 +1,18 @@
+(ns clojure-repl-experiments.slack
+  (:require [clj-slack.channels :as channels]
+            [clj-slack.users :as users]))
+
+;; Note: you can get the quick token by going to the slack web UI, open Chrome DevTools,
+;; click "Elements", find "xoxs-"
+(def clojurians {:api-url "https://slack.com/api" :token "XXX"})
+
+(defn clojure-channel []
+  (->> (channels/list clojurians)
+       :channels
+       (filter #(= "clojure" (:name %)))
+       (take 1)
+       (map (juxt :id :name))
+       ))
+
+
+#_(clj-slack.channels/history clojurians "C03S1KBA2")
