f16c31601e3653772cddd00102e53b7a951d9ee1
Juraj Martinka <jumarko@gmail.com>
2017-11-29 23:21:44 +0100

    Skeleton for jdk related experiments - e.g. jol tool.

---
+++ src/clojure_repl_experiments/jdk.clj
@@ -0,0 +1,6 @@
+(ns clojure-repl-experiments.jdk)
+
+;;; jol - Java Object Layout tool: http://openjdk.java.net/projects/code-tools/jol/
+;;; http://central.maven.org/maven2/org/openjdk/jol/jol-cli
+;;; org.openjdk.jol/jol-core
+
