6b7fa61d4fad500742140de5ab13a7cdec1608c6
Juraj Martinka <jumarko@gmail.com>
2017-12-28 10:01:43 +0100

    More shell experiments with conch.
    * Use `clojure.java.io/reader` instead of verbose java APIs
    * Try to do interactive reading and writing - not working yet.

--- src/clojure_repl_experiments/shell.clj
+++ src/clojure_repl_experiments/shell.clj
@@ -1,6 +1,7 @@
 (ns clojure-repl-experiments.shell
   (:require [me.raynes.conch :refer [programs with-programs let-programs] :as sh]
-            [me.raynes.conch.low-level :as low-sh]))
+            [me.raynes.conch.low-level :as low-sh]
+            [clojure.java.io :as io]))
 
 ;;; Running shell commands from Clojure
 ;;; `conch` library is useful and more flexible than `clojure.java.shell`
@@ -22,8 +23,33 @@
 
 (defn read-command-out []
   (let [process (low-sh/proc "tail" "-f" "/var/log/system.log")]
-    (with-open [process-output-reader (java.io.BufferedReader. (java.io.InputStreamReader. (:out process)))]
+    (with-open [process-output-reader (io/reader (:out process))]
       (read-until-done process-output-reader))
     (println "Process exit code: " (.exitValue (:process process)))))
 #
 #_(read-command-out)
+
+;; run process in the context of given directory
+(print (low-sh/stream-to-string (low-sh/proc "ls" "-l" :dir "/bin") :out))
+
+
+;;; real-time interactive IO
+;;; motivated by noisesmith's comment on slack:
+;;; "* also ymmv but I found ProcessBuilder and Process to be more flexible
+;;;  and easier to use for advanced purposes compared to conch that is,
+;;;  the built in java classes that conch is built on
+;;;  for example I never figured out how to get real-time interactive IO with conch
+;;;  (send string to a process, read result, send another string in reply, etc.),
+;;;  it is straightforward with ProcessBuilder and Process classes"
+(defn write-and-read [command-vector]
+  (let [process (apply low-sh/proc command-vector)]
+    (with-open [process-input-writer (io/writer (:in process))
+                process-output-reader (io/reader (:out process))]
+      (println "WRITING...")
+      (.write process-input-writer (str "Hello cat!" \newline))
+      (println "READING...")
+      (.readLine process-output-reader))
+    (println "Process exit code: " (.exitValue (:process process)))))
+
+;; doesn't work for some reason - it blocks on `.readLine`
+#_(write-and-read ["cat"])
