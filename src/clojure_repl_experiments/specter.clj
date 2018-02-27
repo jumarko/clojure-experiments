6dbbe3af20464c5e134f56a988392c01aceb67f4
Juraj Martinka <jumarko@gmail.com>
2018-02-27 08:40:51 +0100

    Update specter examples.

--- src/clojure_repl_experiments/specter.clj
+++ src/clojure_repl_experiments/specter.clj
@@ -1,5 +1,7 @@
 (ns clojure-repl-experiments.specter
-  (:require [com.rpl.specter :refer [ALL MAP-VALS transform]]))
+  (:require [com.rpl.specter
+             :refer
+             [ALL MAP-VALS setval srange transform traverse-all]]))
 
 ;;; Understanding Specter: Clojure's missing piece
 ;;; https://www.youtube.com/watch?v=rh5J4vacG98&feature=youtu.be
@@ -55,3 +57,27 @@
 [{:a 1 :b 3} {:c 3} {:d 5}]
 
 
+
+;;; http://nathanmarz.com/blog/clojures-missing-piece.html
+;;; manipulating data via the composition of navigators should be a fundamental skill for all functional programmers.
+
+(setval [:a (srange 2 4)] [] {:a [1 2 3 4 5]})
+;;=> {:a [1 2 5]}
+
+(transform [:a (srange 1 5)] reverse {:a [1 2 3 4 5 6]})
+;;=> {:a [1 5 4 3 2 6]}
+
+;; Transducers
+;; Using Vanilla Clojure
+(transduce
+ (comp (map :a) (mapcat identity) (filter odd?))
+ +
+ [{:a [1 2]} {:a [3]} {:a [4 5]}])
+;; => 9
+
+;; The same logic expressed with Specter
+(transduce
+ (traverse-all [:a ALL odd?])
+ +
+ [{:a [1 2]} {:a [3]} {:a [4 5]}])
+;;=> 9
