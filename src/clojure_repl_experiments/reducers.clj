8e4521cea675906c8b4d8f854be245924c4fa6ff
Juraj Martinka <jumarko@gmail.com>
2018-03-29 10:28:12 +0200

    Very brief reducers example.

---
+++ src/clojure_repl_experiments/reducers.clj
@@ -0,0 +1,17 @@
+(ns clojure-repl-experiments.reducers
+  (:require [clojure.core.reducers :as r]))
+
+;;;; https://functional.works-hub.com/learn/reducers-and-transducers-introductory-d0cff
+
+;; this works:
+(nth (map inc [1 2 3]) 2)
+
+;; but reducers returns "reducible"
+#_(nth (r/map inc [1 2 3]) 2)
+;;=> UnsupportedOperationException
+
+(->> [1 2 3]
+     (r/map inc)
+     (into []))
+
+
