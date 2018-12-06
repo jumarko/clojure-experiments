9bca9e026eae9b507bc6773b75735199cc13c45a
Juraj Martinka <jumarko@gmail.com>
2018-12-06 11:24:53 +0100

    datafy/nav examples with files/directories adopted from Stu's REBL talk.

--- src/clojure_repl_experiments/1.10/02_datafy.clj
+++ src/clojure_repl_experiments/1.10/02_datafy.clj
@@ -1,6 +1,9 @@
 (ns clojure-repl-experiments.1.10.02_datafy
-  (:require [clojure.datafy :as d]
-            [clojure.java.data :as java-data]))
+  (:require [clojure.core.protocols :as p]
+            [clojure.datafy :as d]
+            [clojure.java.data :as java-data]
+            [clojure.java.io :as io])
+  (:import java.io.File))
 
 ;;;; datafy
 ;;;; https://clojureverse.org/t/what-use-cases-do-you-see-for-clojure-datafy/3143
@@ -24,4 +27,82 @@
 ;;                               :exception-types [],
 ;;                               :flags #{:public :native}}]])
 
-(d/nav)
+
+
+;;; try with files
+;;; For now, we have to extend File
+
+(defn datafy-node
+  [^File f]
+  {:name (.getName f)
+   :path (.getPath f)
+   :modified (.lastModified f)})
+  
+(def mega (* 1024 1024))
+
+(defn bounded-slurp "like slurp, but only up to approximately limit chars."
+  [f chars-limit]
+  (let [sw (java.io.StringWriter.)]
+    (with-open [rdr (io/reader f)]
+      (let [^"[C" buffer (make-array Character/TYPE 8192)]
+        (loop [n 0]
+          (when (< n chars-limit)
+            (let [size (.read rdr buffer)]
+              (when (pos? size)
+                (do
+                  (.write sw buffer 0 size)
+                  (recur (+ n size)))))))))
+    (.toString sw)))
+
+(defn datafy-file
+  ([^File f] (datafy-file f mega))
+  ([^File f chars-limit]
+   (with-meta
+     (assoc (datafy-node f)
+            :length (.length f)
+            :contents "<elided>")
+     {`p/nav (fn [f k v]
+               (if (= :contents k)
+                 (bounded-slurp (:path f) chars-limit)
+                 v))})))
+
+(defn datafy-directory
+  [^File f]
+  ;; notice that original definition in the talk duplicated `:name` and `:path`,
+  ;; probably for no reason
+  (assoc (datafy-node f)
+         ;; note that we don't want to recursively call `datafy` on children
+         ;; because this would be eager traversal of the whole directory tree
+         ;; :files (map d/datafy (into [] (.listFiles f)))
+         :files (into [] (.listFiles f))))
+
+
+(extend-protocol p/Datafiable
+  java.io.File
+  (datafy [f]
+    (cond
+      (.isFile f) (datafy-file f)
+      (.isDirectory f) (datafy-directory f)
+      :default f)))
+(io/file "/Users/jumar/tools/clojure/")
+;; VS:
+(d/datafy (io/file "/Users/jumar/tools/clojure/"))
+
+;; let's try `nav` now:
+;; quite a lot of work just to navigate to file :)
+#_(let [my-file
+      (->> "/Users/jumar/tools/clojure/"
+           io/file
+           d/datafy
+           :files
+           (filter #(= "rebl" (:name (d/datafy %))))
+           first
+
+           d/datafy
+           :files
+           (filter #(= "deps.edn" (:name (d/datafy %))))
+           first
+
+           d/datafy
+           )]
+  (d/nav my-file :contents (:contents my-file)))
