760a73d1d3a8452aa72cd0c4d3a12469247ee04d
Juraj Martinka <jumarko@gmail.com>
2018-12-19 11:22:24 +0100

    Lazy seqs examples.

---
+++ src/clojure_repl_experiments/lazy.clj
@@ -0,0 +1,16 @@
+(ns clojure-repl-experiments.lazy
+  "Experiments with lazy sequences and related stuff.")
+
+;;; how to properly construct lazy seq?
+(defn lazy-range
+  ([] (lazy-range 0))
+  ([n]
+   (lazy-seq 
+    (cons n (lazy-range (inc n))))))
+
+;; another approach - differences?
+(defn lazy-range
+  ([] (lazy-range 0))
+  ([n]
+   (cons n (lazy-seq (lazy-range (inc n))))))
+#_(lazy-range 100)
