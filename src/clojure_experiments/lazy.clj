6ecf8ce8dcce355d58e05065c932ef8260968974
Juraj Martinka <jumarko@gmail.com>
2018-12-22 22:39:33 +0100

    Copy lazy quicksort to lazy.clj

--- src/clojure_experiments/lazy.clj
+++ src/clojure_experiments/lazy.clj
@@ -14,3 +14,28 @@
   ([n]
    (cons n (lazy-seq (lazy-range (inc n))))))
 #_(lazy-range 100)
+
+
+;;; lazy quick sort - see `clojure-experiments.joy-of-clojure.ch06`
+(defn sort-parts [work]
+  (lazy-seq
+   (loop [[part & parts] work]
+     (if-let [[pivot & xs] (seq part)]
+       (let [smaller? #(< % pivot)]
+         (recur (list*
+                 (filter smaller? xs)
+                 pivot
+                 (remove smaller? xs)
+                 parts)))
+       (when-let [[x & parts] parts]
+         (cons x (sort-parts parts)))))))
+
+(defn lazy-qsort [xs]
+  (sort-parts (list xs)))
+
+(lazy-qsort [2 1 4 3])
+
+(defn rand-ints [n]
+  (take n (repeatedly #(rand-int n))))
+
+#_(lazy-qsort (rand-ints 20))
