bdc257e97a5f2b419bdbdaaa8ed17bf54db98230
Juraj Martinka <jumarko@gmail.com>
2019-01-30 12:47:21 +0100

    Tennis kata: simplify tests for "simple game" and add more of them.

--- src/clojure_experiments/dojo-katas/tennis.clj
+++ src/clojure_experiments/dojo-katas/tennis.clj
@@ -5,9 +5,9 @@
 (defn score
   "Returns human-readable score of the game.
   E.g. '0-15', 'deuce', 'Player1 has an advantage', 'Player1 won the game'."
-  [game-board]
-  (let [[_ player1-score] (first game-board)
-        [_ player2-score] (second game-board)]
+  [game]
+  (let [[_ player1-score] (first game)
+        [_ player2-score] (second game)]
     (format "%s-%s" player1-score player2-score)))
 
 ;; TODO: how about the idea to track points and only translate them to the score in the `score` function?
@@ -16,8 +16,8 @@
     (+ 15 current-score)
     (+ 10 current-score)))
 
-(defn wins-ball [game-board player]
-  (update game-board player update-score))
+(defn wins-ball [game player]
+  (update game player update-score))
 
 (defn make-game [player1-name player2-name]
   ;; here we rely on ordering of small hashmaps which is generally bad but in this case it's completely safe
--- test/clojure_experiments/dojo-katas/tennis_test.clj
+++ test/clojure_experiments/dojo-katas/tennis_test.clj
@@ -3,7 +3,7 @@
             [clojure.test :refer [deftest is testing use-fixtures]]))
 
 (defn new-game []
-  (tennis/make-game "John Doe" "John Smith"))
+  (tennis/make-game "P1" "P2"))
 
 ;;; TODO: fixture later
 ;; check https://stackoverflow.com/questions/31735423/how-to-pass-a-value-from-a-fixture-to-a-test-with-clojure-test
@@ -12,23 +12,44 @@
 
 ;; (use-fixtures :each prepare-game-fixture)
 
+;; TODO: This feels it should be easier (maybe `iterate` or `reduce`?) but it doesn't matter much after all
+(defn- score-n-times [game player-name n]
+  (loop [new-game game
+         cnt n]
+    (if (zero? cnt)
+      new-game
+      (recur (tennis/wins-ball new-game player-name)
+             (dec cnt)))))
 
-(deftest simple-game
-  (testing "Zero score"
-    (is (= "0-0"
-           (tennis/score (new-game)))))
-  (testing "Player 1 scores"
-    (let [game (-> (new-game) (tennis/wins-ball "John Doe"))]
-      (is (= "15-0"
-             (tennis/score game)))))
-  (testing "Player 1 scores three times in a row"
-    (let [game (-> (new-game) (tennis/wins-ball "John Doe") (tennis/wins-ball "John Doe") (tennis/wins-ball "John Doe"))]
-      (is (= "40-0"
-             (tennis/score game))))))
+(defn- check-score [[player1-name player1-points] [player2-name player2-points] expected-score]
+  (testing (format "When player 1 scores %s times and player 2 scores %s times then we expect score: %s"
+                   player1-points player2-points expected-score)
+    (let [game (new-game)
+          player1-game (score-n-times game player1-name player1-points)
+          player2-game (score-n-times player1-game player2-name player2-points)
+          end-state player2-game]
+      (is (= expected-score
+             (tennis/score end-state))))))
+
+(deftest simple-game-first-player
+  (check-score ["P1" 0] ["P2" 0] "0-0")
+  (check-score ["P1" 1] ["P2" 0] "15-0")
+  (check-score ["P1" 2] ["P2" 0] "30-0")
+  (check-score ["P1" 3] ["P2" 0] "40-0"))
+
+(deftest simple-game-second-player
+  (check-score ["P1" 0] ["P2" 1] "0-15")
+  (check-score ["P1" 0] ["P2" 3] "0-40"))
+
+(deftest simple-game-both-players
+  (check-score ["P1" 1] ["P2" 1] "15-15")
+  (check-score ["P1" 1] ["P2" 2] "15-30")
+  (check-score ["P1" 2] ["P2" 3] "30-40"))
 
 ;;; TODO Tests categories
 
 ;; player wins
+
 
 ;; deuce
 
