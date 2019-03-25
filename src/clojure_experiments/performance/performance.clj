bcfb52a35b39855c29c2ba50f865b3b4cfafa7d4
Juraj Martinka <jumarko@gmail.com>
2019-03-25 14:27:47 +0100

    Performance playground.

--- src/clojure_experiments/performance/performance.clj
+++ src/clojure_experiments/performance/performance.clj
@@ -29,8 +29,14 @@
 (comment
 
   (prof/start {})
+  ;; or 
+  (prof/start {:event :alloc})
+
   (burn-cpu 10)
+
   (prof/stop {})
+;; => Execution error (ExceptionInfo) at clj-async-profiler.core/stop (core.clj:262).
+;;    Profiler is not active
   ;; => /private/tmp/clj-async-profiler/results/flamegraph-2019-03-10-12-12-46.svg
   ;; (open in Chrome)
 
@@ -69,4 +75,55 @@
        (recur (unchecked-dec i) (unchecked-add sum i)))))
 
   ;; end comment
+  )
+
+
+;;; https://clojurefun.wordpress.com/2012/08/06/clojure-performance-tip-exploiting-primitive-casts-20/
+(comment
+
+  
+  (defn length1 [x y z]
+    (Math/sqrt (+ (* x x) (* y y) (* z z))))
+  (time (dotimes [i 1000000] (length1 10 10 10)))
+
+  (defn length2 [x y z]
+    (let [x (double x)
+          y (double y)
+          z (double z)]
+      (Math/sqrt (+ (* x x) (* y y) (* z z)))))
+  (time (dotimes [i 1000000] (length2 10 10 10)))
+
+  (defn length3 [^double x ^double y ^double z]
+    (Math/sqrt (+ (* x x) (* y y) (* z z))))
+  (time (dotimes [i 1000000] (length3 10.0 10.0 10.0))))
+
+
+
+;;; Time measuring performance
+;;; https://web.archive.org/web/20160308031939/https://blogs.oracle.com/dholmes/entry/inside_the_hotspot_vm_clocks
+;;; https://shipilev.net/blog/2014/nanotrusting-nanotime/
+
+(comment 
+  (doseq [x (range 1000)]
+    (println "x: " (System/currentTimeMillis)))
+
+  (require '[criterium.core :as crit])
+
+  (crit/quick-bench     (System/currentTimeMillis))
+  ;; Evaluation count : 15211470 in 6 samples of 2535245 calls.
+  ;; Execution time mean : 34.717222 ns
+  ;; Execution time std-deviation : 2.461151 ns
+  ;; Execution time lower quantile : 32.458668 ns ( 2.5%)
+  ;; Execution time upper quantile : 37.786261 ns (97.5%)
+  ;; Overhead used : 6.270366 ns
+
+  (crit/quick-bench     (System/nanoTime))
+  ;; Evaluation count : 13578144 in 6 samples of 2263024 calls.
+  ;; Execution time mean : 41.184378 ns
+  ;; Execution time std-deviation : 3.742258 ns
+  ;; Execution time lower quantile : 37.631263 ns ( 2.5%)
+  ;; Execution time upper quantile : 45.147612 ns (97.5%)
+  ;; Overhead used : 6.270366 ns
+
+  ;; end 
   )
