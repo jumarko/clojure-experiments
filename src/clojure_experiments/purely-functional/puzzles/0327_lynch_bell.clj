e942c36ff1cdbf2a204848027361c1ff21ac4cd2
Juraj Martinka <jumarko@gmail.com>
2019-05-28 17:32:50 +0200

    PF.tv puzzle #327: Lynch Bell numbers.

--- src/clojure_experiments/purely-functional/puzzles/0327_lynch_bell.clj
+++ src/clojure_experiments/purely-functional/puzzles/0327_lynch_bell.clj
@@ -6,7 +6,7 @@
   E.g. 135 is divisible by all 1, 3, 5.
   Note: The number can't contain 0 since division by zero leads to the arithmetic exception.")
 
-;;; The largest one can be 123456789
+;;; The largest one could be 123456789
 
 (defn- digits [n]
   "Returns all digits of given number"
@@ -22,9 +22,29 @@
 
 (defn lynch-bell? [n]
   (let [digs (digits n)
-        rems (map #(rem n %) digs)]
+        nonzero-digs (remove zero? digs)
+        rems (map #(rem n %) nonzero-digs)]
     (every? zero? rems)))
 
-(lynch-bell? 135)
-(lynch-bell? 137)
+(defn generate-lynch-bells [start]
+  (let [max-possible-lynch-bell 987654321]
+    (->> (range (inc max-possible-lynch-bell) start -1)
+         (filter lynch-bell?))))
+
+(defn max-lynch-bell []
+  (first (generate-lynch-bells 1)))
+
+(time (max-lynch-bell ))
+;; => 987653520
+;; 0.55736 msecs
+
+(comment 
+  (lynch-bell? 135)
+  ;; => true
+    
+  (lynch-bell? 137)
+  ;; => false
+
+  ;; eoc
+  )
 
