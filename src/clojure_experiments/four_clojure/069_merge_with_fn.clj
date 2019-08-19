b7aa3c2cc2f38993cc5269569159c1a0afbe5573
Juraj Martinka <jumarko@gmail.com>
2019-08-19 07:07:21 +0200

    4clojure - problem 69 merge-with - second try (refactored based on `clojure.core/merge-with`.

--- src/clojure_experiments/four_clojure/069_merge_with_fn.clj
+++ src/clojure_experiments/four_clojure/069_merge_with_fn.clj
@@ -4,17 +4,13 @@
   Solutions: http://www.4clojure.com/problem/solutions/69")
 
 (defn my-merge-with [f & ms]
-  (reduce
-   (fn [result m]
-     (reduce-kv
-      (fn [result-m m-k m-v]
-        (if-let [[_result-k result-v] (find result-m m-k)]
-          (assoc result-m m-k (f result-v m-v))
-          (assoc result-m m-k m-v)))
-      result
-      m))
-   {}
-   ms))
+  (let [merge-entry (fn [result k v]
+                         (if-let [[_result-k result-v] (find result k)]
+                           (assoc result k (f result-v v))
+                           (assoc result k v)))
+        merge-maps (fn [m1 m2]
+                     (reduce-kv merge-entry m1 m2))]
+    (reduce merge-maps {} ms)))
 
 (my-merge-with * {:a 2, :b 3, :c 4} {:a 2} {:b 2} {:c 5})
 ;; => should be: {:a 4, :b 6, :c 20}
