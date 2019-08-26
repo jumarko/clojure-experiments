256ef6f0c88f5a7b921eac34c3a4b47810a09edf
Juraj Martinka <jumarko@gmail.com>
2019-08-26 10:53:50 +0200

    4Clojure 102 - Camel Case.
    First draft using custom "capitalize" function.

---
+++ src/clojure_experiments/four_clojure/102_camel_case.clj
@@ -0,0 +1,27 @@
+(ns clojure-experiments.four-clojure.102-camel-case
+  "http://www.4clojure.com/problem/102.
+  Write a function that converts hyphen-separated strings to camelCase strings;
+  e.g.'keys-like-this' into 'keysLikeThis'."
+  (:require
+   [clojure.string]
+   [clojure.test :refer [deftest is testing]]))
+
+(defn camel-case
+  [s]
+  (let [capitalize (fn [word] (apply str
+                                     (Character/toUpperCase (first word))
+                                     (rest word)))
+        [first-word & next-words] (clojure.string/split s #"-")
+        capitalized (map capitalize next-words)]
+    (apply str first-word capitalized)))
+
+(deftest camel-case-test
+  (testing "single world isn't capitalized"
+    (is (= "something"
+           (camel-case "something"))))
+  (testing "multi-word key from problem definition"
+    (is (= "multiWordKey"
+           (camel-case "multi-word-key"))))
+  (testing "already capitalized string doesn't change"
+    (is (= "leaveMeAlone"
+           (camel-case "leaveMeAlone")))))
