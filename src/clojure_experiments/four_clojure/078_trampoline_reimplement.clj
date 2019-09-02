ac333898e48679aace1927178e4330cf0699b92d
Juraj Martinka <jumarko@gmail.com>
2019-09-02 07:39:04 +0200

    4Clojure 78: Second and third implementation.
    The third one is the cleanest.
    Also added clojure.core's implementation.

--- src/clojure_experiments/four_clojure/078_trampoline_reimplement.clj
+++ src/clojure_experiments/four_clojure/078_trampoline_reimplement.clj
@@ -1,6 +1,7 @@
 (ns clojure-experiments.four-clojure.078-trampoline-reimplement
   "Reimplement trampoline.
-  http://www.4clojure.com/problem/78")
+  http://www.4clojure.com/problem/78.
+  Solutions: http://www.4clojure.com/problem/solutions/78")
 
 (defn my-trampoline [f & args]
   (let [fval (apply f args)]
@@ -8,6 +9,22 @@
       (if (fn? g)
         (recur (g))
         g))))
+
+(defn my-trampoline2 [f & args]
+  (loop [f f
+         args args]
+    (let [fval (apply f args)]
+      (if (fn? fval)
+        (recur fval [])
+        fval))))
+
+;; no need for extra let to 'apply' args!
+;; (leetwinski's solution)
+(defn my-trampoline3 [f & args]
+  (loop [fval (apply f args)]
+    (if (fn? fval)
+      (recur (fval))
+      fval)))
 
 ;;; From Problem 76:
 (letfn
@@ -17,4 +34,15 @@
                   #(foo x (+ 2 y))))]
   (my-trampoline foo [] 1))
 ;; => [1 3 5 7 9 11]
+
+
+;;; Original `trampoline` implementation from clojure.core
+(defn trampoline 
+  ([f]
+   (let [ret (f)]
+     (if (fn? ret)
+       (recur ret)
+       ret)))
+  ([f & args]
+   (trampoline #(apply f args))))
 
