329eb89afd1c86a38112ba45e768a9be6f01b3f4
Juraj Martinka <jumarko@gmail.com>
2019-09-20 13:13:56 +0200

    Core.Async: more experiments with pending puts

--- src/clojure_experiments/core_async.clj
+++ src/clojure_experiments/core_async.clj
@@ -43,7 +43,7 @@
   ;; 
   )
 
-;; but even non-blocking puts can block the current thread if the 'put queue' is too long?
+;; but even asynchronous puts can block the current thread if the 'put queue' is too long?
 (comment
   (let [ch (a/chan)] (a/go (dotimes [i 10000] (a/put! ch i)))
        (dotimes [i 3] (println (a/<!! ch))))
@@ -56,19 +56,42 @@
 	;; at clojure.core.async.impl.channels.ManyToManyChannel.put_BANG_(channels.clj:152)
 	;; at clojure.core.async$put_BANG_.invokeStatic(async.clj:165)
 
-  ;; we can do better with a/>! which "parks"
+  ;; we can do better with a/>! which "parks" (BUT only if there's no room in the buffer!)
   ;; until the consumer is able to take the value from the channel
-  (let [ch (a/chan)]
+  (let [ch (a/chan 2)]
     (a/go (dotimes [i 10000]
             (a/>! ch i)
             (println "Value" i "put onto a channel")))
     (dotimes [i 3] (println (a/<!! ch))))
     ;; Value 0 put onto a channel
-    ;; 0
-    ;; 1
     ;; Value 1 put onto a channel
     ;; Value 2 put onto a channel
+    ;; 0
+    ;; 1
     ;; 2
+    ;; Value 3 put onto a channel
+    ;; Value 4 put onto a channel
+
+  ;; but a/>! doesn't save us if we span too many processes?
+  ;; Number of exceptions you'll get can vary, but it shouldn't be more
+  ;; than 6 (1024 + 6 = 1030)
+  ;; - usually I got at least 5 exceptions
+  (let [ch (a/chan)]
+    (dotimes [i 1030]
+      (a/go 
+        ;; with little sleeping we generally get only 3 exceptions since the next block
+        ;; will have time to consume 3 values from the channel
+        #_(Thread/sleep 1)
+        (a/>! ch i))
+      #_(println "Value" i "put onto a channel"))
+    (dotimes [i 3] (println (a/<!! ch))))
+  ;; Exception in thread "async-dispatch-29" Exception in thread "async-dispatch-28"
+  ;;   java.lang.AssertionError: Assert failed: No more than 1024 pending puts are allowed on a single channel. Consider using a windowed buffer.
+  ;; (< (.size puts) impl/MAX-QUEUE-SIZE)
+	;; at clojure.core.async.impl.channels.ManyToManyChannel.put_BANG_(channels.clj:152)
+	;; at clojure.core.async.impl.ioc_macros$put_BANG_.invokeStatic(ioc_macros.clj:992)
+  ;; ...
+
 
   ;; 
   )
