4fd559e115a3137e257bfde0414f70fa3c7042f5
Juraj Martinka <jumarko@gmail.com>
2019-10-02 21:03:58 +0200

    Finally working version of set-level! for clojure.tools.logging.

--- src/clojure_experiments/logging.clj
+++ src/clojure_experiments/logging.clj
@@ -1,9 +1,10 @@
 (ns clojure-experiments.logging
   (:require [clojure.spec.alpha :as s]
-            [clojure.tools.logging :as logging]
-            [taoensso.timbre :as log])
+            [clojure.tools.logging :as log]
+            [clojure.tools.logging.impl :as log-impl]
+            [taoensso.timbre :as log-timbre])
   (:import (org.apache.logging.log4j Level LogManager)
-           (org.apache.logging.log4j Level)))
+           (org.apache.logging.log4j.core.config Configurator)))
 
 ;;;; clojure.tools.logging
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -27,6 +28,25 @@
      :off Level/OFF
      :all Level/ALL}
   )
+
+(defn to-log4j-level [level-keyword]
+  (get (log-levels) level-keyword))
+
+;; copied from `clojure.tools.logging.impl/find-factory` and making `log4j2-factory
+;; highest priority (rather than cl-factory and slf4j-factory - commons-logging e.g. is included in apache http client)
+(defn find-factory
+  "Returns the first non-nil value from slf4j-factory, cl-factory,
+   log4j2-factory, log4j-factory, and jul-factory."
+  []
+  (or (log-impl/log4j2-factory)
+      (log-impl/slf4j-factory)
+      (log-impl/cl-factory)
+      (log-impl/log4j-factory)
+      (log-impl/jul-factory)
+      (throw ; this should never happen in 1.5+
+       (RuntimeException.
+        "Valid logging implementation could not be found."))))
+
 (s/def ::log-level (set (keys (log-levels))))
 (s/fdef set-level!
   :args (s/cat :level ::log-level)
@@ -34,37 +54,44 @@
 (defn set-level!
   "Sets new log level for the Root logger. "
   [level]
-  (if-let [log-level (get (log-levels) level)]
-    ;; Using log4j2 API for setting log level: https://stackoverflow.com/a/23434603/1184752
-    (let [logger-context (LogManager/getContext false)
-          logger-config  (-> logger-context
-                             .getConfiguration
-                             (.getLoggerConfig LogManager/ROOT_LOGGER_NAME))]
-      (.setLevel logger-config log-level)
-      ;; This causes all Loggers to refetch information from their LoggerConfig.
-      (.updateLoggers logger-context)
+  (if-let [log-level (to-log4j-level level)]
+    ;; How do I set a logger's level programmaticaly?  https://logging.apache.org/log4j/log4j-2.4/faq.html#reconfig_level_from_code
+    (do
+      (Configurator/setRootLevel log-level)
       ;; finally, we need to update logger-factory used internally by clojure.tools.logging
       ;; otherwise it would cache the log level set when it was initialized
-      (alter-var-root #'logging/*logger-factory* (fn [_] (clojure.tools.logging.impl/find-factory))))
+      (alter-var-root #'log/*logger-factory* (constantly  (find-factory))))
     (throw (IllegalArgumentException. (str "Invalid log level: " level)))))
+
 
 (comment
 
-  ;; nothing should be printed
-  (logging/debug "ahoj")
-  ;; this should be printed
-  (logging/error "ahoj")
+  (Configurator/setRootLevel (to-log4j-level :debug))
+  ;; alternatively you can try `setAllLevels`
+  ;; (Configurator/setAllLevels LogManager/ROOT_LOGGER_NAME (to-log4j-level :info))
 
-  ;; and now it should
-  ;; BUT it's not (there's some problem with SLF4J bridge?; when clojure.tools.logging doesn't autodetect
-  ;; any slf4j dependencies and picks log4j2 instead it works)
-  (set-level! :debug)
-  (logging/debug "ahoj")
+  (do 
+    (set-level! :info)
+    (log/info "PRINTED.")
+
+    (log/debug "NOT PRINTED")
+
+    (set-level! :debug)
+    (log/debug "PRINTED?")
+
+    (set-level! :info)
+    (log/debug "NOT PRINTED")
+
+    (set-level! :warn)
+    (log/info "INFO NOT PRINTED"))
+
+
+  ;; check
+  (log-impl/get-logger log/*logger-factory* *ns*)
+  (log-impl/get-logger (find-factory) *ns*)
 
   ;;
   )
-
-
 
 
 ;;;; Timbre
@@ -114,16 +141,16 @@
   However that is complex and not easy to do (one would have to 'mask' everything in the `:vargs` vector)
   so the easier option using the `:output-fn` was chosen."
   []
-  (log/merge-config!
+  (log-timbre/merge-config!
    {:output-fn (comp
                 mask-secrets-in-log
-                log/default-output-fn)}))
+                log-timbre/default-output-fn)}))
 
 (comment
   ;; Compare
-  (log/info "Bearer XYZ")
+  (log-timbre/info "Bearer XYZ")
   
   (mask-secrets-log-config)
-  (log/info "Bearer XYZ")
+  (log-timbre/info "Bearer XYZ")
   )
 
