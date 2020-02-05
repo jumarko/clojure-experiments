d1635a828d6cb306a2616bd6c41b3882e2895da5
Juraj Martinka <jumarko@gmail.com>
2020-02-05 11:26:02 +0100

    SICP - Ch2 - picture language - Frames section + exercises.

--- src/clojure_experiments/books/sicp/ch2_abstractions_data/s2_picture_language.clj
+++ src/clojure_experiments/books/sicp/ch2_abstractions_data/s2_picture_language.clj
@@ -4,11 +4,11 @@
 
 
 ;;; First define basic combinations
-(declare beside) ; places first painter in the left half
-(declare below) ; places first painter below the second one
-(declare wave)
-(declare flip-vert)
-(declare flip-horiz)
+(defn beside) ; places first painter in the left half
+(defn below) ; places first painter below the second one
+(defn wave)
+(defn flip-vert)
+(defn flip-horiz)
 
 ;;; then we can start combining them
 (def wave2 (beside wave (flip-vert wave)))
@@ -20,7 +20,7 @@
     (below painter2 painter2)))
 
 ;; and we can redefine wave4 using `flipped-pairs`
-(declare flipped-pairs)
+(defn flipped-pairs)
 (def wave4 (flipped-pairs wave))
 
 ;;; We can also define recursive operations (p.131 & 132)
@@ -33,7 +33,7 @@
               (below smaller smaller)))))
 
 ;; notice this uses `up-split` which hasn't been defined yet (ex. 2.44)
-(declare up-split)
+(defn up-split)
 (defn corner-split [painter n]
   (if (zero? n)
     painter
@@ -96,7 +96,105 @@
       painter 
       (let [smaller (split-n painter (dec n))]
         (orig-placer painter
-           (below smaller smaller))))))
+                     (split-placer smaller smaller))))))
 
 (def right-split (split beside below))
 (def up-split (split below beside))
+
+
+;;; Frames (p. 134 - 136)
+;;; Frames are represented by 3 vectors
+;;; - 'origin vector' represents the relative position of the "origin of the frame"
+;;;   from some absolute origin (0,0)
+;;; - 'edge1 and edge2 vectors' represent edges of the frame (can be rectangular or a general parallelogram)
+;;;
+;;; 'frame coordinate map' maps unit square (used to specify images)
+;;; to the frame as follows:
+;;;    Origin(Frame) + x * Edge1(Frame) + y * Edge2(Frame)
+
+;; to start with we only need "constructor" and "selectors"
+(defn make-frame)
+(defn origin-frame)
+(defn edge1-frame)
+(defn edge2-frame)
+
+;; for frame-coord-map we also need vector operations
+(defn add-vect)
+(defn scale-vect)
+(defn xcor-vect)
+(defn ycor-vect)
+
+(defn frame-coord-map [frame]
+  (fn [a-vector]
+    (add-vect (origin-frame frame)
+              (add-vect (scale-vect (xcor-vect a-vector) (edge1-frame))
+                        (scale-vect (ycor-vect a-vector) (edge1-frame))))))
+
+;; Ex. 2.46 (p. 136)
+;; Implement two-dimensional vector v running from the origin to a pint
+;; with a pair consisting of x and y corrdinates.
+;; Create an abstraction via `make-vect`, `xcor-vect` and `ycor-vect`
+;; Then implement `add-vect`, `sub-vect`, `scale-vect`
+(defn make-vect [x y]  [x y])
+(defn xcor-vect [[x _]] x)
+(defn ycor-vect [[_ y]] y)
+
+(ycor-vect (make-vect 10 -100))
+;; => -100
+
+(defn add-vect [v1 v2]
+  (make-vect
+   (+ (xcor-vect v1) (xcor-vect v2))
+   (+ (ycor-vect v1) (ycor-vect v2))))
+(add-vect (make-vect 1 100) (make-vect 10 -100))
+;; => [11 0]
+
+(defn sub-vect [v1 v2]
+  (make-vect
+   (- (xcor-vect v1) (xcor-vect v2))
+   (- (ycor-vect v1) (ycor-vect v2))))
+(sub-vect (make-vect 1 100) (make-vect 10 -100))
+;; => [-9 200]
+
+(defn scale-vect [scalar v]
+  (make-vect
+   (* scalar (xcor-vect v))
+   (* scalar (ycor-vect v))))
+(scale-vect 5 (make-vect 10 -100))
+;; => [50 -500]
+
+;; Now I'll try to reimplement add-vect and sub-vect using a more general procedure `combine-vect`
+(defn combine-vect [op v1 v2]
+  (make-vect
+   (op (xcor-vect v1) (xcor-vect v2))
+   (op (ycor-vect v1) (ycor-vect v2))))
+(def add-vect (partial combine-vect +))
+(def sub-vect (partial combine-vect -))
+(add-vect (make-vect 1 100) (make-vect 10 -100))
+;; => [11 0]
+(sub-vect (make-vect 1 100) (make-vect 10 -100))
+;; => [-9 200]
+
+
+;; Ex. 2.47 (p. 136)
+;; Provide selectors for given constructors:
+
+;; 1st constructor
+(defn make-frame [origin edge1 edge2]
+  (list origin edge1 edge2))
+(defn origin-frame [f]
+  (first f))
+(defn edge1-frame [f]
+  (second f))
+(defn edge2-frame [f]
+  (nth f 3))
+
+;; 2nd constructor
+(defn make-frame [origin edge1 edge2]
+  (cons origin (cons edge1 edge2)))
+(defn origin-frame [f]
+  (first f))
+(defn edge1-frame [f]
+  (first (second f)))
+(defn edge2-frame [f]
+  (second (second f)))
