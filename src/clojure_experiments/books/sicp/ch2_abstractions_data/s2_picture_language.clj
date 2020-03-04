9f1fe995c4c1161b5fe9cba2e14bb93ede0138eb
Juraj Martinka <jumarko@gmail.com>
2020-03-04 11:07:42 +0100

    SICP: wrap up picture language (section 2.2).

--- src/clojure_experiments/books/sicp/ch2_abstractions_data/s2_picture_language.clj
+++ src/clojure_experiments/books/sicp/ch2_abstractions_data/s2_picture_language.clj
@@ -6,11 +6,11 @@
 
 
 ;;; First define basic combinations
-(defn beside) ; places first painter in the left half
-(defn below) ; places first painter below the second one
-(defn wave)
-(defn flip-vert)
-(defn flip-horiz)
+(defn beside [x y]) ; places first painter in the left half
+(defn below [x y]) ; places first painter below the second one
+(defn wave [])
+(defn flip-vert [x])
+(defn flip-horiz [x])
 
 ;;; then we can start combining them
 (def wave2 (beside wave (flip-vert wave)))
@@ -22,7 +22,7 @@
     (below painter2 painter2)))
 
 ;; and we can redefine wave4 using `flipped-pairs`
-(defn flipped-pairs)
+(defn flipped-pairs [x])
 (def wave4 (flipped-pairs wave))
 
 ;;; We can also define recursive operations (p.131 & 132)
@@ -35,7 +35,7 @@
               (below smaller smaller)))))
 
 ;; notice this uses `up-split` which hasn't been defined yet (ex. 2.44)
-(defn up-split)
+(defn up-split [n])
 (defn corner-split [painter n]
   (if (zero? n)
     painter
@@ -79,13 +79,16 @@
   (square-of-four painter (flip-vert painter)
                   painter (flip-vert painter)))
 ;; or even easier!!
-(def flipped-pairs identity flip-vert identity flip-vert)
+;; Note: this is incorrect
+;; (def flipped-pairs identity flip-vert identity flip-vert)
 
 ;; and now redefine `square-limit` using `square-of-four`:
+(declare rotate180)
+(declare squre-of-four)
 (defn square-limit [painter n]
   (let [combine4 (square-of-four flip-horiz identity
                                  rotate180 flip-vert)]
-    (combine4 (cornert-split painter n)))
+    (combine4 (corner-split painter n)))
   )
 
 
@@ -108,7 +111,7 @@
 ;;; Frames are represented by 3 vectors
 ;;; - 'origin vector' represents the relative position of the "origin of the frame"
 ;;;   from some absolute origin (0,0)
-;;; - 'edge1 and edge2 vectors' represent edges of the frame (can be rectangular or a general parallelogram)
+;;; - 'edge1 and e2 vectors' represent edges of the frame (can be rectangular or a general parallelogram)
 ;;;
 ;;; 'frame coordinate map' maps unit square (used to specify images)
 ;;; to the frame as follows:
@@ -182,8 +185,8 @@
 ;; Provide selectors for given constructors:
 
 ;; 1st constructor
-(defn make-frame [origin edge1 edge2]
-  (list origin edge1 edge2))
+(defn make-frame [origin edge1 e2]
+  (list origin edge1 e2))
 (defn origin-frame [f]
   (first f))
 (defn edge1-frame [f]
@@ -192,8 +195,8 @@
   (nth f 3))
 
 ;; 2nd possible constructor
-(defn make-frame [origin edge1 edge2]
-  (cons origin (cons edge1 edge2)))
+(defn make-frame [origin edge1 e2]
+  (cons origin (cons edge1 e2)))
 (defn origin-frame [f]
   (first f))
 (defn edge1-frame [f]
@@ -280,3 +283,145 @@
   (make-vect 50 50)
   (make-vect 200 90)
   (make-vect 10 90)))
+
+
+
+;;; Ex 2.48
+(defn make-segment [start end]
+  [start end])
+(def start-segment first)
+(def end-segment second)
+
+;;; Ex. 2.49 (p. 137)
+;; a. The painter that draws the outline of the designated frame
+(defn frame-painter [frame]
+  (let [a (origin-frame frame)
+        e1 (edge1-frame frame)
+        e2 (edge2-frame frame)
+        b (add-vect a e1)
+        c (add-vect b e2)
+        d (add-vect a e2)]
+    (segments->painter
+     [(make-segment a b)
+      (make-segment b c)
+      (make-segment c d)
+      (make-segment d a)])))
+
+
+;;; Transforming and combining painters (p. 138)
+;;; flip-vert  doesn't need to know how painters work,
+;;; just how to turn a frame upside down.
+
+(defn transform-painter
+  "Produced a new painter which calls the original painter
+  on a transformed frame defined by new origin and corners."
+  [painter origin corner1 corner2]
+  (fn [frame]
+    (let [m (frame-coord-map frame)
+          new-origin (m origin)
+          new-corner1 (sub-vect (m corner1) new-origin)
+          new-corner2 (sub-vect (m corner2) new-origin)]
+      (painter
+       (make-frame new-origin new-corner1 new-corner2)))))
+
+;; we can then define flip-vert easily
+(defn flip-vert [painter]
+  (transform-painter painter
+                     (make-vect 0.0 1.0) ; new origin
+                     (make-vect 1.0 1.0) ;new end of edge1
+                     (make-vect 0.0 0.0) ;new end of edge1
+                     ))
+
+;; we can also shrink to upper right corner
+(defn shrink-to-upper-right [painter]
+  (transform-painter painter
+                     (make-vect 0.5 0.5)
+                     (make-vect 1.0 0.5 )
+                     (make-vect 0.5 1.0)))
+
+
+(defn rotate90 [painter]
+  (transform-painter painter
+                     (make-vect 1.0 0.0)
+                     (make-vect 1.0 1.0)
+                     (make-vect 0.0 0.0)))
+
+;; or squash towards the center of the frame
+(defn squash-inwards [painter]
+  (transform-painter painter
+                     (make-vect 0.0 0.0)
+                     (make-vect 0.65 0.35)
+                     (make-vect 0.35 0.65)))
+
+;; and finally beside
+(defn beside [painter1 painter2]
+  (let [split-point (make-vect 0.5 0.0)
+        paint-left (transform-painter painter1
+                                      (make-vect 0.0 0.0)
+                                      split-point
+                                      (make-vect 0.0 1.0))
+        paint-right (transform-painter painter2
+                                       split-point
+                                       (make-vect 1.0 0.0)
+                                       (make-vect 0.5 1.0))]
+    (fn [frame]
+      (paint-left frame)
+      (paint-right frame))))
+
+
+;;; Ex. 2.50 (p. 140)
+;;; Define the transformation flip-horiz which flips painters horizontally
+;;; and transformations that rotate painters counterclockwise by 180 degrees
+;;; and 270 degrees.
+(defn flip-horiz
+  "Flips the painter horizontally."
+  [painter]
+  (transform-painter painter
+                     (make-vect 1.0 0.0) ; new origin
+                     (make-vect 0.0 0.0) ;new end of edge1
+                     (make-vect 1.0 1.0) ;new end of edge2
+                     ))
+
+(defn rotate180 [painter]
+  (transform-painter painter
+                     (make-vect 1.0 1.0) ; new origin
+                     (make-vect 0.0 1.0) ;new end of edge1
+                     (make-vect 1.0 0.0) ;new end of edge2
+                     ))
+(defn rotate270 [painter]
+  (transform-painter painter
+                     (make-vect 0.0 1.0) ; new origin
+                     (make-vect 0.0 0.0) ;new end of edge1
+                     (make-vect 1.0 1.0) ;new end of edge2
+                     ))
+
+;;; Ex. 2.51 (p. 140 & p.131)
+;;; Define below operation
+;;; Draw it using two different ways
+;;; 1. analogous to the `beside` implementation
+;;; 2. using `beside` and rotations from Ex 2.50
+
+;; 1. as `beside`
+(defn below1 [painter-bottom painter-top]
+  (let [split-point (make-vect 0.0 0.5)
+        paint-bottom (transform-painter painter-bottom
+                                      (make-vect 0.0 0.0)
+                                      (make-vect 0.0 1.0)
+                                      split-point)
+        paint-top (transform-painter painter-top
+                                       split-point
+                                       (make-vect 1.0 0.5)
+                                       (make-vect 0.0 1.0))]
+    (fn [frame]
+      (paint-bottom frame)
+      (paint-top frame))))
+
+
+;; 2. using `beside` and rotations
+(defn below2 [painter-bottom painter-top]
+  (rotate90
+   (beside (rotate270 painter-top) (rotate270 painter-bottom))))
+;; or 
+(defn below2 [painter-bottom painter-top]
+  (rotate270
+   (beside (rotate90 painter-top) (rotate90 painter-bottom))))
