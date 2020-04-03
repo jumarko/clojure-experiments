f43420fb79a9ee8f59384b13e6ad0e63c05dbbae
Juraj Martinka <jumarko@gmail.com>
2020-04-03 10:42:02 +0200

    (unsigned) right-shift experiments

--- src/clojure_experiments/math_and_numbers.clj
+++ src/clojure_experiments/math_and_numbers.clj
@@ -15,3 +15,20 @@
 ;; => true
 
 
+
+;; right shift in java: >> (bit-shift-right) vs. >>> (unsigned-bit-shift-right)
+;; motivated by the Computer Systems book (p. 94)
+
+
+(bit-shift-right 8 2)
+;; => 2
+(unsigned-bit-shift-right 8 2)
+;; => 2
+
+(bit-shift-right -8 2)
+;; => -2
+(unsigned-bit-shift-right -8 2)
+;; => 4611686018427387902
+;; (Long/MAX_VALUE) = 9223372036854775807
+(long (/ Long/MAX_VALUE  2))
+;; => 4611686018427387903
