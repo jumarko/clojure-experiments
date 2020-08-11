98924a30f4e6a480a6e5f6fde0829ac6c16282af
Juraj Martinka <jumarko@gmail.com>
2020-08-11 09:16:04 +0200

    Exclude perc99 from descriptive statistics.
    Not that interesting - usually very similar to perc95 (at least for my aws logs experiments).

--- src/clojure_experiments/stats/descriptive.clj
+++ src/clojure_experiments/stats/descriptive.clj
@@ -6,7 +6,7 @@
 
 (s/fdef describe
   :args (s/cat :data (s/every double?))
-  :ret (s/keys :req-un [::min ::perc25 ::median ::perc75 ::perc95 ::perc99 ::max ::mean ::standard-deviation ::sum]))
+  :ret (s/keys :req-un [::min ::perc25 ::median ::perc75 ::perc95 ::max ::mean ::standard-deviation ::sum]))
 (defn describe
   "Given a sequence/collection of numbers (doubles!)
   returns a map of descriptive statistics. "
@@ -20,19 +20,19 @@
         (assoc :perc25 (perc-fn 25)
                :median (perc-fn 50)
                :perc75 (perc-fn 75)
-               :perc95 (perc-fn 95)
-               :perc99 (perc-fn 99))
-        (select-keys [:min :perc25 :median :perc75 :perc95 :perc99 :max :mean :standard-deviation :sum]))))
+               :perc95 (perc-fn 95))
+        (select-keys [:min :perc25 :median :perc75 :perc95 :max :mean :standard-deviation :sum]))))
 
 (defn describe-as-ints [data]
   (m/map-vals int (describe data)))
 
 (defn describe-as-vector
   "Same as `describe` but returns a simple vector of statistics values preserving order
-  for an easy comparison between multiple sets of statistics."
+  for an easy comparison between multiple sets of statistics.
+  Returns these stats in order: min, perc25, median, perc75, perc95, max, mean, stdev, sum"
   ([data]
    (describe-as-vector describe data))
   ([describe-fn data]
    (->> (describe-fn data)
-        ((juxt :min :perc25 :median :perc75 :perc95 :perc99 :max :mean :standard-deviation :sum)))))
+        ((juxt :min :perc25 :median :perc75 :perc95 :max :mean :standard-deviation :sum)))))
 
