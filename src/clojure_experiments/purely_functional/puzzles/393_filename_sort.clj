a6c36e48de296a9d9edd0c238a18e1f82a7dc2b5
Juraj Martinka <jumarko@gmail.com>
2020-09-03 07:47:00 +0200

    PF.tv puzzle 393 - filename ordering: fix number ordering test.
    The implementation isn't complete because I couldn't find a solution for
    ordering numbers.

--- src/clojure_experiments/purely_functional/puzzles/393_filename_sort.clj
+++ src/clojure_experiments/purely_functional/puzzles/393_filename_sort.clj
@@ -17,21 +17,27 @@
       ;; case insensitive
       (str/lower-case)
 
-      ;; proper number ordering -> prefix all numbers with zero
-      (str/replace #"(\d+)" "0$1")
+      ;; proper number ordering?
+      ;; I couldn't find a canonical form to which this could be transformed
+      ;; so that numbers are ordered properly
+      #_(str/replace #"(\d+)" "0$1") ; this clearly doesn't work
 
       ;; accent insensitive
       ;; https://stackoverflow.com/questions/51731574/removing-accents-and-diacritics-in-kotlin
       ;; https://docs.oracle.com/en/java/javase/11/docs/api/java.base/java/text/Normalizer.Form.html
       (Normalizer/normalize Normalizer$Form/NFD)))
 
+
 (deftest test-filename-order
   (testing "Case insensitivity"
     (is (= ["academy.zip" "Zoo.txt"]
            (sort-by filename-order ["Zoo.txt" "academy.zip"]))))
   (testing "Numbers sorting"
-    (is (= ["academy.zip" "Zoo.txt"]
-           (sort-by filename-order ["12 Final chapter.txt" "2 Second chapter.txt"]))))
+    (is (= ["2 Second chapter.txt" "12 Final chapter.txt"]
+           (sort-by filename-order ["12 Final chapter.txt" "2 Second chapter.txt"])))
+    (is (= ["3" "21a" "100a"]
+           (sort-by filename-order ["100a" "3" "21a"]))))
   (testing "Accent insensitivity"
     (is (= ["elena" "Elena" "Élena" "Fero"]
            (sort-by filename-order ["elena", "Elena", "Fero", "Élena"])))))
+
