935e2b459250d77f606064de5f4f7ffaa2652ad8
Juraj Martinka <jumarko@gmail.com>
2020-09-24 05:53:06 +0200

    Joy of Clojure: starting chapter 06 - structural sharing example, etc.

--- src/clojure_experiments/books/joy_of_clojure/ch06.clj
+++ src/clojure_experiments/books/joy_of_clojure/ch06.clj
@@ -1,4 +1,5 @@
-(ns clojure-experiments.books.joy-of-clojure.ch06)
+(ns clojure-experiments.books.joy-of-clojure.ch06
+  (:require [cljol.dig9 :as cljol]))
 
 ;;; lazy quick sort (p. 133) - This is a gem!
 (defn sort-parts [work]
@@ -42,3 +43,59 @@
 #_(time (doall (lazy-qsort numbers)))
 ;; "Elapsed time: 14839.150604 msecs"
  
+
+
+;;; Structural sharing
+
+;; list
+(def baselist (list :barnabas :adam))
+(def lst1 (cons :willie baselist))
+(def lst2 (cons :phoenix baselist))
+
+;; the next parts of both lists are identical
+(= (next lst1) (next lst2))
+;; => true
+(identical? (next lst1) (next lst2))
+;; => true
+#_(cljol/view lst1)
+
+
+;; Let's try to build a simple tree to demonstrate
+;; how structural sharing works
+{:val 5 :L nil :R nil} ; use this when a single item is added to an empty list
+
+(defn xconj
+  "Builds up the tree by adding given value to the tree."
+  [t v]
+  (cond
+    (nil? t) {:val v :L nil :R nil}))
+
+(xconj nil 5)
+;; => {:val 5, :L nil, :R nil}
+
+;; that's fine but we want more than just a single item
+(defn xconj
+  "Builds up the tree by adding given value to the tree."
+  [t v]
+  (cond
+    (nil? t) {:val v :L nil :R nil}
+    (< v (:val t)) {:val (:val t)
+                    :L (xconj (:L t) v)
+                    :R (:R t)}
+    :else {:val (:val t)
+           :L (:L t)
+           :R (xconj (:R t) v)}))
+
+;; we'll need a better way to print the tree
+(defn xseq [t]
+  (when t
+    (concat (xseq (:L t))
+            [(:val t)]
+            (xseq (:R t)))))
+
+(def tree1 (xconj (xconj (xconj nil 5) 3)
+                  2))
+(xseq tree1)
+;; => (2 3 5)
+(xseq (xconj tree1 7))
+;; => (2 3 5 7)
