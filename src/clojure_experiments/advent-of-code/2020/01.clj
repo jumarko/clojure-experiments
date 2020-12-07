549b6ba11170da53e294b61e944e2bcfbcf27e46
Juraj Martinka <jumarko@gmail.com>
2020-12-07 13:40:32 +0100

    Advent of Code 2020: Day 01.

---
+++ src/clojure_experiments/advent-of-code/2020/01.clj
@@ -0,0 +1,55 @@
+(ns clojure-experiments.advent-of-code.2020.01
+  "https://adventofcode.com/2020/day/1.
+  Input: https://adventofcode.com/2020/day/1/input"
+  (:require [clojure.test :refer [deftest is testing]]
+            [clojure.string :as str]))
+
+(defn sum-2020
+  "Finds two numbers in the expense report that sump up to 2020
+  and returns a product of these numbers.
+  Returns nil if no such numbers are present."
+  [expense-report]
+  (let [[x y]  (first (for [x expense-report
+                            y expense-report
+                     :when (= 2020 (+ x y))]
+                       [x y]))]
+    (when x (* x y))))
+
+(def sample-report [1721 979 366 299 675 1456])
+(def test-report (-> (slurp "src/clojure_experiments/advent-of-code/2020/01.txt")
+                     (str/split #"\n")
+                     (as-> $ (mapv #(Integer/parseInt %) $))
+                      vec))
+(sum-2020 test-report)
+;; => 211899
+
+(deftest sum-2020-test
+  (testing "Simple sum"
+    (is (= 514579
+           (sum-2020 sample-report))))
+  )
+
+
+;; Note: this could be possibly optimized by sorting the input
+;; and than only taking numbers larger than given into consideration?
+(defn sum-2020-3
+  "Finds three numbers in the expense report that sump up to 2020
+  and returns a product of these numbers.
+  Returns nil if no such numbers are present."
+  [expense-report]
+  (let [[x y z]  (first (for [x expense-report
+                              y expense-report
+                              z expense-report
+                            :when (= 2020 (+ x y z))]
+                        [x y z]))]
+    (when x (* x y z))))
+
+(time (sum-2020-3 test-report))
+;; "Elapsed time: 139.808565 msecs"
+;; => 275765682
+
+(deftest sum-2020-test-3
+  (testing "Simple sum"
+    (is (= 241861950
+           (sum-2020-3 sample-report))))
+  )
