992227eb647fb099c2e8587687a0d82893dc6a9c
Juraj Martinka <jumarko@gmail.com>
2021-01-13 05:31:21 +0100

    Joy of Clojure: Chapter 12 - gen-class and GUI programming.

--- src/clojure_experiments/books/joy_of_clojure/ch12_java_next.clj
+++ src/clojure_experiments/books/joy_of_clojure/ch12_java_next.clj
@@ -3,7 +3,11 @@
             [clojure.string :as string])
   (:import [com.sun.net.httpserver HttpHandler HttpExchange HttpServer]
            [java.net InetSocketAddress URLDecoder URI]
-           [java.io File FilterOutputStream]))
+           [java.io File FilterOutputStream]
+           (javax.swing Box BoxLayout JTextField JPanel
+                        JSplitPane JLabel JButton JOptionPane)
+           (java.awt BorderLayout Component Container FlowLayout GridLayout)
+           (java.awt.event ActionListener)))
 
 (def OK java.net.HttpURLConnection/HTTP_OK)
 
@@ -119,3 +123,125 @@
           (respond html-around exchange (html-links (str uri) (listing file))))
       (respond exchange (details file)))))
 #_(update-proxy p {"handle" fs-handler})
+
+
+;;; gen-class and gui programming (p. 286 - 292)
+
+;; must be standalone namespace?
+#_(ns joy.gui.DynaFrame
+  (:gen-class
+   :name joy.gui.DynaFrame
+   :extends javax.swing.JFrame
+   :implements [clojure.lang.IMeta]
+   :prefix "df-" ; must be string
+   :state state
+   :init init
+   ;; mapping of the class constructors to the superclass constructors
+   :constructors {[String] [String]
+                  [] [String]}
+   :methods [[display [java.awt.Container] void]
+             ^{:static true} [version [] String]])
+  (:import (javax.swing JFrame JPanel JComponent)
+           (java.awt BorderLayout Container)))
+
+(compile 'clojure-experiments.books.joy-of-clojure.DynaFrame)
+;; => clojure-experiments.books.joy-of-clojure.DynaFrame
+;; notice that the actual name of the class is `joy.gui.DynaFrame` as stated in `:name` 
+#_(joy.gui.DynaFrame. "First try")
+;; Execution error (UnsupportedOperationException) at joy.gui.DynaFrame/<init> (REPL:-1).
+;; clojure-experiments.books.joy-of-clojure.DynaFrame/df-init not defined
+
+#_(joy.gui.DynaFrame. "Second try")
+;; meta (clojure-experiments.books.joy-of-clojure.DynaFrame/df-meta not defined?)
+
+(meta (joy.gui.DynaFrame. "Third try"))
+;; => #:clojure-experiments.books.joy-of-clojure.DynaFrame{:title "Third try"}
+
+;; note that this doesn't work - it's because my ns is named differently from the generated class
+;; (joy.gui.DynaFrame/version)
+;; No such var: joy.gui.DynaFrame/version
+
+;; but using dot-special operator works
+;; and notice that the function name must be with prefix too, that is `df-version`
+(. joy.gui.DynaFrame version)
+;; => "1.0"
+
+(def gui (joy.gui.DynaFrame. "4th"))
+
+#_(.display gui (doto (JPanel.)
+                (.add (JLabel. "Charlemagne and Pippin"))))
+
+
+;;; Let's build 3 GUI containers - shelves, stacks, splitters
+(defn shelf [& components]
+  (let [shelf (JPanel.)]
+    (.setLayout shelf (FlowLayout.))
+    (doseq [c components]
+      (.add shelf c))
+    shelf))
+
+(defn stack [& components]
+  (let [stack (Box. BoxLayout/PAGE_AXIS)]
+    (doseq [c components]
+      (.setAlignmentX c Component/CENTER_ALIGNMENT)
+      (.add stack c))
+    stack))
+
+(defn splitter [top bottom]
+  (doto (JSplitPane.)
+    (.setOrientation JSplitPane/VERTICAL_SPLIT)
+    (.setLeftComponent top)
+    (.setRightComponent bottom)))
+
+;; let's now provide basic widgets: buttons, labels, text boxes
+(defn button [text f]
+  (doto (JButton. text)
+    ;; use reify instead of proxy: https://stackoverflow.com/questions/5821892/why-should-i-use-reify-instead-of-proxy-in-clojure
+    #_(.addActionListener (proxy [ActionListener] [] (actionPerformed [_] (f))))
+    (.addActionListener
+     (reify ActionListener
+       (actionPerformed [this _] (f))))))
+
+(defn txt [cols t]
+  (doto (JTextField.)
+    (.setColumns cols)
+    (.setText t)))
+
+(defn label [txt] (JLabel. txt))
+
+(defn alert
+  ([msg] (alert nil msg))
+  ([frame msg]
+   (JOptionPane/showMessageDialog frame msg)))
+
+#_(.display gui (splitter
+               (button "Procrastinate" #(alert "Eat Cheetos"))
+               (button "Move It" #(alert "Couch to 5k"))))
+
+;; let's add one more widget builder
+(defn grid [x y f]
+  (let [g (doto (JPanel.)
+            (.setLayout (GridLayout. x y)))]
+    (dotimes [_ (* x y)]
+      (.add g (f)))
+    g))
+
+(defn rich-ui [dyna-frame]
+  (.display
+   dyna-frame
+   (let [g1 (txt 10 "Charlemagne")
+         g2 (txt 10 "Pippin")
+         r (txt 3 "10")
+         d (txt 3 "5")]
+     (splitter
+      (stack
+       (shelf (label "Player 1") g1)
+       (shelf (label "Player 2") g2)
+       (shelf (label "Rounds ") r (label "Delay  ") d))
+      (stack
+       (grid 21 11 #(label "-"))
+       (button "Go1" #(alert (format "%s vs. %s for %s rounds, every %s seconds."
+                                     (.getText g1) (.getText g2) (.getText r) (.getText d)))))))))
+#_(rich-ui gui)
+
+
