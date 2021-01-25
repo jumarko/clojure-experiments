5ccb8f0962f94acc67d15c09fe57e6bbd1ab23c7
Juraj Martinka <jumarko@gmail.com>
2021-01-25 07:40:17 +0100

    Statistics distributions skeleton.

---
+++ src/clojure_experiments/stats/distributions.clj
@@ -0,0 +1,7 @@
+(ns clojure-experiments.stats.distributions
+  "See https://michaellindon.github.io/software/distributions/.
+  Github: https://github.com/michaellindon/distributions"
+  (:require [distributions.normal :as d]))
+
+
+
