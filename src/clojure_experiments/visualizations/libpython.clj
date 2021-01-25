cd00ac6825389f899d1936f647e25696c75f6fa0
Juraj Martinka <jumarko@gmail.com>
2021-01-25 07:42:33 +0100

    libpython visualizations skeleton.

---
+++ src/clojure_experiments/visualizations/libpython.clj
@@ -0,0 +1,16 @@
+(ns clojure-experiments.visualizations.libpython
+  (:require [libpython-clj.require :refer [require-python]]
+            [libpython-clj.python :as py :refer [py. py.. py.-]]
+            [clojure.java.shell :as sh]))
+
+;;; Gigasquid's examples: https://github.com/gigasquid/libpython-clj-examples
+;;; - e.g. https://github.com/gigasquid/libpython-clj-examples/blob/master/src/gigasquid/plot.clj
+
+;;; How to use plotly
+;;; https://plotly.com/python/getting-started/
+(comment
+  
+;;; ???
+  (py/import-module "plotly.express")
+  (require-python '[plotly.express :as plotly]))
+
