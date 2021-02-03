85ea5ff1067c7238990f314ef439b0a30c9a75d9
Juraj Martinka <jumarko@gmail.com>
2021-02-03 05:24:22 +0100

    Joy of Clojure - Ch. 14 section about event sourcing finished.

--- src/clojure_experiments/books/joy_of_clojure/ch14_data_oriented_programming.clj
+++ src/clojure_experiments/books/joy_of_clojure/ch14_data_oriented_programming.clj
@@ -1,8 +1,8 @@
 (ns clojure-experiments.books.joy-of-clojure.ch14-data-oriented-programming
-  (:require [clojure.data :as d]
+  (:require [clojure-experiments.books.joy-of-clojure.ch07-fp :as ch7]
+            [clojure.data :as d]
             [clojure.edn :as edn]
-            [clojure-experiments.books.joy-of-clojure.ch07-fp :as ch7]))
-
+            [clojure.set :as set]))
 
 ;;; Useful discussion of Place-Oriented Programming in the first pages
 ;;; * Place oriented programming is fundamentally limited - it destroys time and thus information by default (special care needed to preserve the history)
@@ -228,3 +228,134 @@
 ;;     {:ab 8, :h 4, :avg 0.5}
 ;;     {:ab 9, :h 4, :avg 0.4444444444444444}
 ;;     {:ab 10, :h 4, :avg 0.4})
+
+
+;;; Simulation Testing (p. 352 - 357)
+
+(def PLAYERS #{{:player "Nick" :ability 8/25}
+               {:player "Matt" :ability 13/50}
+               {:player "Ryan" :ability 19/100}})
+
+(defn lookup [db name]
+  ;; note: they use alias `sql` for set in the book
+  (first (set/select #(= name (:player %))
+                     db)))
+
+(lookup PLAYERS "Nick")
+;; => {:player "Nick", :ability 8/25}
+
+(defn update-stats [db event]
+  (let [player (lookup db (:player event))
+        ;; they used `set/difference` here; I just use `disj`
+        less-db (disj db player)]
+    (conj less-db
+          (merge player (effect player event)))))
+
+
+(update-stats PLAYERS {:player "Nick" :result :hit})
+;; => #{{:player "Matt", :ability 13/50} {:player "Nick", :ability 8/25, :ab 1, :h 1, :avg 1.0}
+;;      {:player "Ryan", :ability 19/100} {:player "Nick", :ability 8/25}}
+
+;; => #{{:player "Matt", :ability 13/50}
+;;      {:player "Nick", :ability 8/25, :ab 1, :h 1, :avg 1.0}
+;;      {:player "Ryan", :ability 19/100}}
+
+(defn commit-event [db event]
+  #_(prn "Update db with event: " event)
+  #_(prn "db before: " db)
+  (let [db-after (dosync (alter db update-stats event))]
+    #_(prn "db after: " db)
+    db-after)
+)
+
+(commit-event (ref PLAYERS) {:player "Nick" :result :hit})
+;; => #{{:player "Matt", :ability 26} {:player "Ryan", :ability 19} {:player "Nick", :ability 32, :ab 1, :h 1, :avg 1.0}}
+
+;; now to finally demonstrate simulation testing
+(defn rand-event [{ability :ability}]
+  (let [able (numerator ability)
+        max (denominator ability)]
+    (rand-map 1
+              #(-> :result)
+              #(if (< (rand-int max) able)
+                 :hit
+                 :out))))
+
+(defn rand-events [total player]
+  (take total
+        (repeatedly #(assoc (rand-event player)
+                            :player
+                            (:player player)))))
+
+(rand-events 3 {:player "Nick" :ability 32/100})
+;; => ({:result :out, :player "Nick"} {:result :hit, :player "Nick"} {:result :out, :player "Nick"})
+
+
+;; Now we can use agents to simulate players
+;; - our domain maps nicely onto the notion of autonomous actors operating asynchronously
+
+(def agent-for-player
+  ;; memoize here allows you to use `player-name` as an index to a table of agents
+  ;; without having to explicitly manage and look up agents in the table!
+  (memoize
+   (fn [player-name]
+     (doto (agent [])
+         (set-error-handler! #(println "ERROR: " %1 %2))
+         (set-error-mode! :fail)))))
+
+(defn feed
+  "Apply given event to the database (should be a ref).
+  Returns an agent that will perform the update."
+  [db event]
+  (let [a (agent-for-player (:player event))]
+    ;; I think send-off would make more sense if we were making changes to a real database
+    (send a
+          (fn [state]
+            (commit-event db event)
+            (conj state event)))))
+
+(defn feed-all [db events]
+  (doseq [event events]
+    (feed db event))
+  db)
+
+(let [db (ref PLAYERS)]
+  (feed-all db (rand-events 100 {:player "Nick" :ability 32/100}))
+  ;; Thread/sleep is needed otherwise I don't see much updates
+  (Thread/sleep 10)
+  db)
+;; => #<Ref@6fe285c: 
+;;      #{{:player "Matt", :ability 13/50}
+;;        {:player "Ryan", :ability 19/100}
+;;        {:player "Nick", :ability 8/25, :ab 100, :h 33, :avg 0.33}}>
+
+;; you can apply the events manually to check your expectations
+(effect-all {} @(agent-for-player "Nick"))
+;; this is the result when you experiment with it and invoke it more than once
+; (memoize remembers the previous invocations)
+; => {:ab 230, :h 40, :avg 0.3076923076923077}
+
+
+;; top-level `simulate` function to generate events for all players
+;; and apply them in "interleaved" order
+(defn simulate [total-per-player players]
+  (let [events (apply interleave
+                      (for [player players] (rand-events total-per-player player)))
+        results (feed-all (ref players) events)]
+    (apply await (map #(agent-for-player (:player %))
+                      players))
+    @results))
+
+(simulate 2 PLAYERS)
+;; => #{{:player "Matt", :ability 13/50, :ab 2, :h 0, :avg 0.0}
+;;      {:player "Nick", :ability 8/25, :ab 2, :h 0, :avg 0.0}
+;;      {:player "Ryan", :ability 19/100, :ab 2, :h 0, :avg 0.0}}
+
+(simulate 400 PLAYERS)
+;; => #{{:player "Ryan", :ability 19/100, :ab 400, :h 77, :avg 0.1925}
+;;      {:player "Matt", :ability 13/50, :ab 400, :h 101, :avg 0.2525}
+;;      {:player "Nick", :ability 8/25, :ab 400, :h 125, :avg 0.3125}}
+
+;; you can as well check with effect-all:
+(effect-all {} @(agent-for-player "Nick"))
+
