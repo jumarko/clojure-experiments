b6e03ee328d8c541519acd3b99206724de964346
Juraj Martinka <jumarko@gmail.com>
2021-02-18 06:01:12 +0100

    Joy of Clojure - Ch 15 - add a copy of clojure.core.cache.CacheProtocol

--- src/clojure_experiments/books/joy_of_clojure/ch15_performance.clj
+++ src/clojure_experiments/books/joy_of_clojure/ch15_performance.clj
@@ -222,6 +222,30 @@
   (hit [cache e] "Called when the item is found in the cache. Should return the cache object itself.")
   (miss [cache e ret] "Called when the item isn't found in the cache. Should return the cache object itself."))
 
+;; Note: here's the clojure.core.cache.CacheProtocol
+(comment
+  (defprotocol CacheProtocol
+    "This is the protocol describing the basic cache capability."
+    (lookup [cache e]
+      [cache e not-found]
+      "Retrieve the value associated with `e` if it exists, else `nil` in
+   the 2-arg case.  Retrieve the value associated with `e` if it exists,
+   else `not-found` in the 3-arg case.")
+    (has?    [cache e]
+      "Checks if the cache contains a value associated with `e`")
+    (hit     [cache e]
+      "Is meant to be called if the cache is determined to contain a value
+   associated with `e`")
+    (miss    [cache e ret]
+      "Is meant to be called if the cache is determined to **not** contain a
+   value associated with `e`")
+    (evict  [cache e]
+      "Removes an entry from the cache")
+    (seed    [cache base]
+      "Is used to signal that the cache should be created with a seed.
+   The contract is that said cache should return an instance of its
+   own type.")))
+
 (deftype BasicCache [cache]
   CacheProtocol
   (lookup [_ item]
