cb1484cde4d39c36edcd5c8e13aa4d61a7c86dd5
Juraj Martinka <jumarko@gmail.com>
2021-12-07 08:16:25 +0100

    clj-decompiler examples.

--- src/clojure_experiments/decompiler.clj
+++ src/clojure_experiments/decompiler.clj
@@ -11,3 +11,28 @@
 ;;                          :output-path "/Users/jumar/tools/clojure/rebl/REBL-0.9.108/src"})
 
 
+
+;; http://clojure-goes-fast.com/blog/introspection-tools-java-decompilers/
+(require '[clj-java-decompiler.core :refer [decompile disassemble] :as decompiler])
+;; see https://insideclojure.org/2014/12/15/warn-on-boxed/
+
+;; 1. unchecked math using specific operations
+(decompile
+ (loop [i 100, sum 0] (if (< i 0)
+                        sum
+                        (recur (unchecked-dec i) (unchecked-add sum i)))))
+
+;; 2. checked math - notice using Numbers.dec, Numbers.add, and Numbers.num => wrappers!
+(decompile
+ (loop [i 100, sum 0] (if (< i 0)
+                        sum
+                        (recur (dec i) (+ sum i)))))
+
+;; 3. Compiling using unchecked-math is the same as the first case
+(set! *unchecked-math* true)
+(decompile
+ (loop [i 100, sum 0] (if (< i 0)
+                        sum
+                        (recur (dec i) (+ sum i)))))
+(set! *unchecked-math* false)
+
