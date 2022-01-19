3799122bde4fed6606d90bb5a834b478bad584f1
Juraj Martinka <jumarko@gmail.com>
2022-01-19 10:40:14 +0100

    Add <<- macro.

--- src/clojure_experiments/macros/core.clj
+++ src/clojure_experiments/macros/core.clj
@@ -31,3 +31,54 @@
 (def foo 1)
 (defmacro foo* [] (list 'quote foo))
 (defn bar [] (+ (foo*) 10))
+
+
+
+;;; <-- macro https://clojurians.slack.com/archives/C03S1KBA2/p1642537427215800
+;;; Christophe Grand: https://twitter.com/cgrand/status/1281527501387440128
+(defmacro <<- [& forms] `(->> ~@(reverse forms)))
+
+(defn fn-which-may-return-falsey [x]
+  (or x false))
+(defn fn-which-may-return-nil [x]
+  (or x nil))
+
+(let [a 10
+      y 100]
+  (<<-
+   (if (odd? a) 1)
+   (let [a (quot a 2)])
+   (when-let [x (fn-which-may-return-falsey a)])
+   (when-some [b (fn-which-may-return-nil x)])
+   (when (pos? x))
+   ;; this is tricky: it prints '5' with `do` but '5 2' without `do`!
+   (do (println x))
+   (if (odd? (+ x y)) 2)
+   3))
+;; => 2
+
+;; macroexpansion:
+(let*
+    [a 10 y 100]
+  (if (odd? a)
+    1
+    (let*
+        [a (quot a 2)]
+      (let*
+          [temp__5756__auto__ (fn-which-may-return-falsey a)]
+        (if temp__5756__auto__
+          (do
+            (let*
+                [x temp__5756__auto__]
+              (let*
+                  [temp__5760__auto__ (fn-which-may-return-nil x)]
+                (if (nil? temp__5760__auto__)
+                  nil
+                  (let*
+                      [b temp__5760__auto__]
+                    (if (pos? x)
+                      (do
+                        (do
+                          (println x)
+                          (if (odd? (+ x y)) 2 3))))))))))))))
+;; => 2
