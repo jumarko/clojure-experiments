08b07d39cc38f2cc3da50ecdbe14cb8af3c35d74
Juraj Martinka <jumarko@gmail.com>
2022-02-23 06:33:27 +0100

    New debugging experiments.

---
+++ src/clojure_experiments/debugging.clj
@@ -0,0 +1,23 @@
+(ns clojure-experiments.debugging
+  "Experiments with debugging code,
+  perhaps using Cider debugger: https://docs.cider.mx/cider/debugging/debugger.html")
+
+
+(defn baz [z]
+  (let [zz (+ 10 z)
+        zzs (repeat zz)]
+    (mapv inc zzs)))
+
+(defn bar [y]
+  (let [yy (+ 5 y)]
+    (baz yy)))
+
+(defn foo [x]
+  (bar (inc x)))
+
+
+(comment
+
+  (foo 3)
+
+  ,)
