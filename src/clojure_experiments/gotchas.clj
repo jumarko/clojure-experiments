eb0235e75473620d5a244988eafc4bdf2263b6ab
Juraj Martinka <jumarko@gmail.com>
2022-03-18 09:16:35 +0100

    Add set/union gotcha.

--- src/clojure_experiments/gotchas.clj
+++ src/clojure_experiments/gotchas.clj
@@ -1,5 +1,6 @@
 (ns clojure-experiments.gotchas
-  "Things to watch for when using clojure (core) api.")
+  "Things to watch for when using clojure (core) api."
+  (:require [clojure.set :as set]))
 
 ;;; rseq doesn't support all sequential collections
 ;;; - only vectors and sorted maps
@@ -10,3 +11,9 @@
 ;; 1. Unhandled java.lang.ClassCastException
 ;; class clojure.lang.LazySeq cannot be cast to class clojure.lang.Reversible (clojure.lang.LazySeq
 ;; core.clj: 1596  clojure.core/rseq
+
+
+;;; some set operations, like `clojure.set/union`,
+;;; produce garbage when you pass input that's not a set
+(set/union [1 2 3] #{3 4 5})
+;; => [1 2 3 4 3 5] ; result is a vector with duplicates!!!
