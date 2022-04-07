666fac83775d77b5154c8d1a9179f24af949eec6
Juraj Martinka <jumarko@gmail.com>
2022-04-07 11:19:06 +0200

    Add *print-dup* example from Serializing Clojure objects: https://groups.google.com/g/clojure/c/5wRBTPNu8qo
    2008 discussion with a couple of comments from Rich Hickey

--- src/clojure_experiments/compiler/reader-and-printer.clj
+++ src/clojure_experiments/compiler/reader-and-printer.clj
@@ -30,3 +30,62 @@
 (binding [*read-eval* false]
   (read-string "#=(java.lang.System/exit 0)"))
 
+
+;;; Serializing Clojure objects: https://groups.google.com/g/clojure/c/5wRBTPNu8qo [2008 discussion with a couple of comments from Rich Hickey]
+;;; Rich: Yes, please consider print/read.
+;;; It is readable text, works with a lot of data structures, and is extensible.
+;;; As part of AOT I needed to enhance print/read to store constants of many kinds, and restore faithfully.
+;;;  This led to a new multimethod - print-dup, for high-fidelity printing.
+;;; You can get print-dup behavior by binding *print-dup*
+;;;
+;;; Rich's example:
+(binding [*print-dup* true]
+  (dorun
+   (map prn
+        [[1 2 3]
+         {4 5 6 7}
+         (java.util.ArrayList. [8 9])
+         String
+         "string"
+         42M
+         :hello
+         #"ethel"
+         (sorted-set 9 8 7 6)
+         #'rest])))
+;; prints this:
+;; [1 2 3]
+;; #=(clojure.lang.PersistentArrayMap/create {4 5, 6 7})
+;; #=(java.util.ArrayList. [8 9])
+;; #=java.lang.String
+;; "string"
+;; 42M
+;; :hello
+;; #"ethel"
+;; #=(clojure.lang.PersistentTreeSet/create [6 7 8 9])
+;; #=(var clojure.core/rest)
+
+
+;; Compare to this
+(dorun
+ (map prn
+      [[1 2 3]
+       {4 5 6 7}
+       (java.util.ArrayList. [8 9])
+       String
+       "string"
+       42M
+       :hello
+       #"ethel"
+       (sorted-set 9 8 7 6)
+       #'rest]))
+;;=> prints this:
+;; [1 2 3]
+;; {4 5, 6 7}
+;; [8 9]
+;; java.lang.String
+;; "string"
+;; 42M
+;; :hello
+;; #"ethel"
+;; #{6 7 8 9}
+;; #'clojure.core/rest
