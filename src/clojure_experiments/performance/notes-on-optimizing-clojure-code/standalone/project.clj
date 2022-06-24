553e454ea262cd241d308b1d792e96b87a1f59d5
Juraj Martinka <jumarko@gmail.com>
2022-06-24 09:50:59 +0200

    Notes on optimizing clojure: use clojure 1.11.1 in project.clj instead of 1.10.3

--- src/clojure_experiments/performance/notes-on-optimizing-clojure-code/standalone/project.clj
+++ src/clojure_experiments/performance/notes-on-optimizing-clojure-code/standalone/project.clj
@@ -3,7 +3,7 @@
   :url "https://cuddly-octo-palm-tree.com/posts/2022-01-16-opt-clj-1/"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.3"]]
+  :dependencies [[org.clojure/clojure "1.11.1"]]
   :main ^:skip-aot p01-overview
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
