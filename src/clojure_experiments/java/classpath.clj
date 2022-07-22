e283cb54c02c8eae5a2b9d67a0f0b8435d747167
Juraj Martinka <jumarko@gmail.com>
2022-07-22 10:59:13 +0200

    Simple classpath example.

--- src/clojure_experiments/java/classpath.clj
+++ src/clojure_experiments/java/classpath.clj
@@ -1,5 +1,6 @@
 (ns clojure-experiments.java.classpath
-  (:import (io.github.classgraph ClassGraph)
+  (:require [lambdaisland.classpath :as cp])
+  #_(:import (io.github.classgraph ClassGraph)
            (nonapi.io.github.classgraph.classpath ClasspathFinder)
            (nonapi.io.github.classgraph.scanspec ScanSpec)
            (nonapi.io.github.classgraph.utils LogNode)))
@@ -21,3 +22,24 @@
 
   ;; 
   )
+
+;;; https://lambdaisland.com/blog/2021-08-25-classpath-is-a-lie
+;;; https://github.com/lambdaisland/classpath
+(comment
+
+  (cp/classloader-chain)
+  ;; => (#object[clojure.lang.DynamicClassLoader 0x1d92c596 "clojure.lang.DynamicClassLoader@1d92c596"]
+  ;;     ....
+  ;;     #object[clojure.lang.DynamicClassLoader 0x7d3bbfce "clojure.lang.DynamicClassLoader@7d3bbfce"]
+  ;;     #object[jdk.internal.loader.ClassLoaders$AppClassLoader 0x251a69d7 "jdk.internal.loader.ClassLoaders$AppClassLoader@251a69d7"]
+  ;;     #object[jdk.internal.loader.ClassLoaders$PlatformClassLoader 0x4c194da "jdk.internal.loader.ClassLoaders$PlatformClassLoader@4c194da"])
+
+  ;; inspect classpaths in detail
+  (cp/classpath-chain)
+
+  .)
+
+;; you can also update the classpath!!!
+(comment
+
+  .)
