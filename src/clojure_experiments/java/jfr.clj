fb45770d8dd4615f1e21d504b28af7ec3061525f
Juraj Martinka <jumarko@gmail.com>
2022-07-26 15:41:35 +0200

    Experiment with JFR RecordingStream.

--- src/clojure_experiments/java/jfr.clj
+++ src/clojure_experiments/java/jfr.clj
@@ -2,17 +2,20 @@
   "See https://cr.openjdk.java.net/~egahlin/jep-349/javadocs/api/jdk.jfr/jdk/jfr/consumer/"
   (:import
    (java.nio.file Path)
-   (jdk.jfr.consumer RecordingFile)))
+   (jdk.jfr Configuration)
+   (jdk.jfr.consumer RecordingFile
+                     RecordingStream)))
 
 ;;; https://cr.openjdk.java.net/~egahlin/jep-349/javadocs/api/jdk.jfr/jdk/jfr/consumer/RecordingFile.html
 ;;; https://cr.openjdk.java.net/~egahlin/jep-349/javadocs/api/jdk.jfr/jdk/jfr/consumer/RecordedEvent.html
 
-(def my-recording-file "/Users/jumar/Work/Empear-Codescene/support-issues/SystemVerification/tobias-internal-server-error/jfr.codescene-profiling.2019-10-15_09-07-25.jfr")
+(comment
+  (def my-recording-file "my-jfr-profiling.jfr")
 
-(def my-events
-  (->> (Path/of my-recording-file (make-array String 0))
-      (RecordingFile/readAllEvents)
-      (map bean)))
+  (def my-events
+    (->> (Path/of my-recording-file (make-array String 0))
+         (RecordingFile/readAllEvents)
+         (map bean)))
 
 
 (first my-events)
@@ -26,3 +29,39 @@
 ;;     :startTime #object[java.time.Instant 0x4e5638b9 "2019-10-15T07:38:12.240645921Z"],
 ;;     :thread
 ;;     #object[jdk.jfr.consumer.RecordedThread 0x63283271 "{\n  osName = \"Attach Listener\"\n  osThreadId = 3953\n  javaName = \"Attach Listener\"\n  javaThreadId = 3617\n  group = {\n    parent = N/A\n    name = \"system\"\n  }\n}\n"]}
+
+  )
+
+
+;;; Start recording and listen to events
+;;; RecordingStream: https://docs.oracle.com/en/java/javase/15/docs/api/jdk.jfr/jdk/jfr/consumer/RecordingStream.html
+(defn consumer [accept-fn]
+  (reify
+    java.util.function.Consumer
+    (accept [this obj] (accept-fn obj))))
+
+(defn recording
+  "Starts blocking recording (via start method) using `RecordingStream`.
+  The events will be processes using provided `accept-fn`."
+  ([jfr-configuration accept-fn]
+   (recording jfr-configuration accept-fn ["jdk.CPULoad" "jdk.JVMInformation"]))
+  ([jfr-configuration accept-fn events]
+   (let [consume (consumer accept-fn)]
+     (with-open [rs (RecordingStream. jfr-configuration)]
+       (doseq [e events]
+         (.onEvent rs e consume))
+       ;; https://docs.oracle.com/en/java/javase/15/docs/api/jdk.jfr/jdk/jfr/consumer/EventStream.html#startAsync()
+       (.start rs)))))
+
+(comment
+  ;; blocks and keeps printing the CPULoad events to stdout
+  (recording (Configuration/getConfiguration "default")
+             println)
+
+  ;; collects events in the atom on a separate thread
+  (def my-events (atom []))
+  (def my-recording (future (recording (Configuration/getConfiguration "default")
+                      #(swap! my-events conj %))))
+  .)
+
+
