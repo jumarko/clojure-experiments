e323acdd554eb2849e4628a174fb05217b16e361
Juraj Martinka <jumarko@gmail.com>
2022-08-03 15:03:11 +0200

    Format experiments.

--- src/clojure_experiments/macros/formats/fressian.clj
+++ src/clojure_experiments/macros/formats/fressian.clj
@@ -1,20 +1,28 @@
 (ns clojure-experiments.macros.formats.fressian
   "https://github.com/clojure/data.fressian"
   (:require [clojure.data.fressian :as fress]
-            [clj-memory-meter.core :as mm]))
+            [clojure-experiments.performance.memory :as memory]
+            [clj-memory-meter.core :as mm]
+            [cognitect.transit :as transit])
+  (:import (java.io ByteArrayInputStream
+                    ByteArrayOutputStream)))
 
-;;; https://github.com/clojure/data.fressian#usage
+
+(def my-data {:a (vec (range 1000000))
+              :b (vec (repeat 1000000 "Hello"))})
+(mm/measure my-data)
+;; => "33.2 MiB"
+
+;;; Fressian: https://github.com/clojure/data.fressian#usage
 (comment
-  (def my-data {:a (vec (range 1000000))
-                :b (vec (repeat 1000000 "Hello"))})
-    (mm/measure my-data)
-    ;; => "33.2 MiB"
 
-  (def written (fress/write my-data))
+  (def written (time (fress/write my-data)))
+  ;; "Elapsed time: 404.440516 msecs"
   (mm/measure written)
 ;; => "16.0 MiB"
 
-  (def read-data (fress/read written))
+  (def read-data (time (fress/read written)))
+  ;; "Elapsed time: 374.062715 msecs"
   ;; TODO: wait, why it's so much larger than `my-data`?
   (mm/measure read-data)
   ;; => "76.3 MiB"
