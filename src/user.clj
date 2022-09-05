330084af679b762feb47b0fb54985a7c488399eb
Juraj Martinka <jumarko@gmail.com>
2022-09-05 06:11:52 +0200

    Tech.viz visualizations.
    tech.viz is a really cool library for quick and easy graphs.

--- src/user.clj
+++ src/user.clj
@@ -1,9 +1,15 @@
 (ns user
-  (:require [flow-storm.api :as fs-api]))
+  (:require
+   [flow-storm.api :as fs-api]
+   [portal.api :as p]))
 
 ;; Start FlowStorm debugger in user.clj instead of from the REPL or clj buffer later
 ;; This fixes the problem with being unable to switch to FlowStorm window via Cmd+Tab
 ;; See https://clojurians.slack.com/archives/C03KZ3XT0CF/p1658370905532999?thread_ts=1658305276.475879&cid=C03KZ3XT0CF
 (println "Starting FlowStorm debugger on thread: " (.getName (Thread/currentThread)))
 (fs-api/local-connect)
+
+(def portal (p/open))
+(add-tap #'p/submit)
+
 
