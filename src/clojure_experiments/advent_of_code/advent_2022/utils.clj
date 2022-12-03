36d8559b003bbd4fcd4fd2e17608effd893874cd
Juraj Martinka <jumarko@gmail.com>
2022-12-03 07:42:16 +0100

    Advent of Code - Day 3.

---
+++ src/clojure_experiments/advent_of_code/advent_2022/utils.clj
@@ -0,0 +1,11 @@
+(ns clojure-experiments.advent-of-code.advent-2022.utils
+  "See also `clojure-experiments.advent-of-code.advent-2020.utils`."
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str]))
+
+(defn read-input [day-num]
+  (->> (slurp (io/reader (format "src/clojure_experiments/advent_of_code/advent_2022/day_%s.txt"
+                                 day-num)))
+       str/split-lines))
+
+
