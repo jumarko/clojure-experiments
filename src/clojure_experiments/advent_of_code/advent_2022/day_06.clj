f729f52e4eaa42ebe23a602cc54764ceced74981
Juraj Martinka <jumarko@gmail.com>
2022-12-06 13:21:51 +0100

    Advent of Code - day 6 - alternative implementations.

--- src/clojure_experiments/advent_of_code/advent_2022/day_06.clj
+++ src/clojure_experiments/advent_of_code/advent_2022/day_06.clj
@@ -16,19 +16,19 @@
 ;;; Part 1
 
 (defn packet-start [signal-code]
-  (reduce (fn [[prefix index :as acc] ch]
+  (reduce (fn [[index prefix :as acc] ch]
             (if (= 4 (count (set prefix)))
               ;; they are all unique
               (reduced acc)
-              [(str (subs prefix (if (= 4 (count prefix)) 1 0))
-                    ch)
-               (inc index)]))
-          ["" 0]
+              [(inc index)
+               (str (subs prefix (if (= 4 (count prefix)) 1 0))
+                    ch)]))
+          [0 ""]
           signal-code))
 (packet-start sample-input)
-;; => ["jpqm" 7]
+;; => [7 "jpqm"]
 (defn start-of-packet [signal-code]
-  (second (packet-start signal-code)))
+  (first (packet-start signal-code)))
 
 (assert= 7 (start-of-packet sample-input))
 ;; some more examples:
@@ -47,21 +47,21 @@
 
 ;; let's modify `packet-start` slightly to make the length of the prefix dynamic
 (defn message-start [signal-code prefix-length]
-  (reduce (fn [[prefix index :as acc] ch]
+  (reduce (fn [[index prefix :as acc] ch]
             (if (= prefix-length (count (set prefix)))
               ;; they are all unique
               (reduced acc)
-              [(str (subs prefix (if (= prefix-length (count prefix)) 1 0))
-                    ch)
-               (inc index)]))
-          ["" 0]
+              [(inc index)
+               (str (subs prefix (if (= prefix-length (count prefix)) 1 0))
+                    ch)]))
+          [0 ""]
           signal-code))
 (defn start-of-packet [signal-code]
-  (second (message-start signal-code 4)))
+  (first (message-start signal-code 4)))
 (assert= 11 (start-of-packet "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"))
 
 (defn start-of-message [signal-code]
-  (second (message-start signal-code 14)))
+  (first (message-start signal-code 14)))
 
 (assert= 19 (start-of-message sample-input))
 ;; some more examples:
@@ -73,3 +73,50 @@
 ;; Part 2 solution:
 (assert= 2263 (start-of-message input))
 
+
+
+;;; Try an alternative implementation
+
+;; map-indexed with multiple arguments could work, right?
+;; - except that `map-indexed` doesn't take multiple collections
+;; => so just `map`
+(defn message-start [signal-code prefix-length]
+  (->> (apply map (fn [i & chars] (when (= (count chars) (count (set chars)))
+                                    [i (apply str chars)]))
+              (range prefix-length (count signal-code))
+              (take prefix-length (iterate next signal-code)))
+       (remove nil?)
+       first))
+(message-start sample-input 4)
+;; => [7 "jpqm"]
+(assert= 7 (start-of-packet sample-input))
+(assert= 1134 (start-of-packet input))
+(assert= 2263 (start-of-message input))
+
+
+;; Even simpler version  using `partition`
+(take 4 (partition 4 1 sample-input))
+;; => ((\m \j \q \j) (\j \q \j \p) (\q \j \p \q) (\j \p \q \m))
+
+(defn message-start [signal-code prefix-length]
+  (->> (map-indexed (fn [i chars]
+                      (when (= (count chars) (count (set chars)))
+                        [(+ i prefix-length) (apply str chars)]))
+                    (partition prefix-length 1 signal-code))
+       (remove nil?)
+       first))
+(message-start sample-input 4)
+;; => [7 "jpqm"]
+(assert= 7 (start-of-packet sample-input))
+(assert= 1134 (start-of-packet input))
+(assert= 2263 (start-of-message input))
+
+;; even simpler is to use `keep-indexed`
+(defn message-start [signal-code prefix-length]
+  (first (keep-indexed (fn [i chars]
+                         (when (= (count chars) (count (set chars)))
+                           [(+ i prefix-length) (apply str chars)]))
+                       (partition prefix-length 1 signal-code))))
+(assert= 7 (start-of-packet sample-input))
+(assert= 1134 (start-of-packet input))
+(assert= 2263 (start-of-message input))
