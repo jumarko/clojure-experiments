0f3af4a5cd806f84428e5612ce9a7f338fbdc97a
Juraj Martinka <jumarko@gmail.com>
2024-06-21 12:27:14 +0200

    Can't recur across try.

--- src/clojure_experiments/recursion.clj
+++ src/clojure_experiments/recursion.clj
@@ -12,3 +12,20 @@
     (= i 3) (recur (inc i))
     (> i 0) (recur (dec i))
     :else :done))
+
+;; you cannot recur from within catch block:
+;; - What is the technical or ideological reason for not allowing `recur` from inside a `catch` block? https://ask.clojure.org/index.php/13924/technical-ideological-reason-allowing-recur-inside-catch
+;;   -> try can have a finally which runs after the catch, so you can't recur from inside a catch as it is not necessarily the tail position.
+
+(comment
+  (defn my-try-catch-loop []
+    (loop [i 0]
+      (try
+        (parse-long (str i))
+        (catch Exception e
+          ;; compilation error: Can only recur from tail position
+          (if (< i 10)
+            i
+            (recur (inc i)))))))
+  ;;
+  )
