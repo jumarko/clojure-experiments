d07b630a7d696f6f73a36f67d3068b62ef582b24
Juraj Martinka <jumarko@gmail.com>
2025-12-05 11:01:36 +0100

    Advent of Code 2025 - day 5 - part 2.
    Slightly tricky but relatively easy.
    See https://www.geeksforgeeks.org/dsa/minimum-removals-required-to-make-ranges-non-overlapping/

--- src/clojure_experiments/advent_of_code/advent_2025/day_05.clj
+++ src/clojure_experiments/advent_of_code/advent_2025/day_05.clj
@@ -58,3 +58,50 @@
 
 (part1 full-parsed)
 ;; => 848
+
+
+;;; Part 2: ignore the list of available ingredients;
+;;; Instead, count how many fresh ingredients are in the ranges
+
+;; First naive attept - of course it doesn't work because the ranges can _overlap_.
+(defn part2 [{:keys [fresh-ranges] :as parsed-input}]
+  (reduce
+   (fn [acc [start end]]
+     (+ acc (- end start)))
+   0
+   fresh-ranges))
+(part2 sample-parsed)
+;; => 16 (should be 14)
+
+
+;; try again...
+;; So we must reoncile overlapping intervals somehow
+;; Here are some ideas: https://www.geeksforgeeks.org/dsa/minimum-removals-required-to-make-ranges-non-overlapping/
+;; I think sorting them by starting element works nicely - let's try that!
+(defn non-overlapping-intervals [intervals]
+  (let [sorted-intervals (sort-by first intervals)]
+    (reduce
+     (fn [non-overlapping-intervals [a b :as interval]]
+       (let [[x y :as previous-interval] (peek non-overlapping-intervals)]
+         (if (or (nil? previous-interval) (< y a))
+           ;; non-overlapping intervals, simply add the new interval to the list
+           (conj non-overlapping-intervals interval)
+           ;; overlapping intervals, let's replace previous-interval 
+           (conj (pop non-overlapping-intervals) [x (max y b)]))))
+     []
+     sorted-intervals)))
+
+(assert (= [[3 5] [10 20]]
+           (non-overlapping-intervals (:fresh-ranges sample-parsed))))
+
+(defn part2 [{:keys [fresh-ranges] :as _parsed-input}]
+  (let [non-overlapping-ranges (non-overlapping-intervals fresh-ranges)]
+    (reduce
+     (fn [acc [start end]]
+       (+ acc (inc (- end start))))
+     0
+     non-overlapping-ranges)))
+(assert (= 14 (part2 sample-parsed)))
+
+(part2 full-parsed)
+;; => 334714395325710
