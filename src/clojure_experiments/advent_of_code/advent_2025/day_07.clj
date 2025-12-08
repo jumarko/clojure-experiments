536658c0ae9fd729319db0d818726b7f47b3147b
Juraj Martinka <jumarko@gmail.com>
2025-12-09 00:11:04 +0100

    Advent of Code 2025 - day 7 - part1.
    Non-trivial.

---
+++ src/clojure_experiments/advent_of_code/advent_2025/day_07.clj
@@ -0,0 +1,135 @@
+(ns clojure-experiments.advent-of-code.advent-2025.day-07
+  "Cephalopod math homework.
+  Input: https://adventofcode.com/2025/day/7/input.
+  "
+  (:require
+   [clojure-experiments.advent-of-code.utils :as utils]
+   [clojure.spec.alpha :as s]
+   [clojure.string :as str]))
+
+
+
+(def sample-input (drop 1 (str/split-lines "
+.......S.......
+...............
+.......^.......
+...............
+......^.^......
+...............
+.....^.^.^.....
+...............
+....^.^...^....
+...............
+...^.^...^.^...
+...............
+..^...^.....^..
+...............
+.^.^.^.^.^...^.
+...............")))
+
+(def full-input (utils/read-input 2025 7))
+
+
+;;; Part 1: The goal is to find how many times the tachyom beam is split
+;;; This boils down to the number of '^' in the manifold diagram,
+;;; but counting only those that are actually hit by some beam.
+;;; In the sample input, there's one that doesn't get hit,
+;;; therefore the answer is 21, not 22.
+
+
+;;; Solution method.
+;;; Generate the final diagram with beams ('|') shown in appropriate places.
+;;; We do that by starting with a single beam in the second row directly under the 'S' symbol,
+;;; then com
+;;; by iterating over the input and combining 
+;;; To get the final result (number of beams)
+
+(def splitter \^)
+(def empty-spot \.)
+(def beam \|)
+(defn- combine [previous-row-with-beams next-row-without-beams]
+  (apply str
+         (map-indexed (fn [i previous-char]
+                        (let [next-char (get next-row-without-beams i)
+                              splitter-hit-by-beam? (fn [index]
+                                                      (and (<= 0 index)
+                                                           (= splitter (get next-row-without-beams index))
+                                                           (= beam (get previous-row-with-beams index))))]
+                          (condp = previous-char
+                            ;; there's always empty space after a splitter
+                            splitter empty-spot
+                            beam (if (= next-char empty-spot)
+                                   ;; beam continues if there's empty space after it ...
+                                   beam
+                                   ;; ... and get's blocked if there's a splitter.
+                                   splitter)
+                            ;; the most tricky case - empty spot might be transformed into a beam
+                            ;; if there's a splitter at adjacent positions AND there's a beam above the splitter
+                            empty-spot (cond
+                                         ;; if it's a splitter just repeat it - we cannot replace it with a beam or empty spot
+                                         (= splitter next-char) splitter
+
+                                         ;; check if there's a splitter to the left or right and there was a beam above it
+                                         (or (splitter-hit-by-beam? (inc i))
+                                             (splitter-hit-by-beam? (dec i)))
+                                         beam
+
+                                         :else next-char))))
+
+                      previous-row-with-beams)))
+
+(defn trace-beams
+  [[first-line & rest-lines :as input-lines]]
+  ;; this re-draws the original input diagram with beams at appropriate location
+  ;; starting with the _second_ row (with a beam directly under 'S')
+  (reduce (fn [beams-diagram current-level]
+            (conj beams-diagram (combine (peek beams-diagram) current-level)))
+          ;; Start with one beam at the 'S' position
+          [(str/replace first-line \S \|)]
+          rest-lines))
+
+(trace-beams sample-input)
+;;=>
+[".......|......."
+ ".......|......."
+ "......|^|......"
+ "......|.|......"
+ ".....|^|^|....."
+ ".....|.|.|....."
+ "....|^|^|^|...."
+ "....|.|.|.|...."
+ "...|^|^|||^|..."
+ "...|.|.|||.|..."
+ "..|^|^|||^|^|.."
+ "..|.|.|||.|.|.."
+ ".|^|||^||.||^|."
+ ".|.|||.||.||.|."
+ "|^|^|^|^|^|||^|"
+ "|.|.|.|.|.|||.|"]
+
+;; the answer is the number of '^' with a beam above it
+(defn part1 [parsed-input]
+  (let [traced (trace-beams parsed-input)
+        ;; I call 'inactive' splitters that aren't hit by a beam;
+        ;; as such, they don't split anything and shouldn't be counted.
+        removed-inactive-splitters
+        (map (fn [previous-line line]
+               (map-indexed (fn [i spot]
+                              (if (= splitter spot)
+                                ;; remove the splitter if there isn't a beam above it
+                                ;; to not count those
+                                (when (= beam (get previous-line i)) splitter)
+                                spot))
+                            line))
+             ;; the first line is irrelevant because there cannot be any relevant splitters on the first two lines
+             (drop 1 traced) (drop 2 traced))]
+    ;; now count the number of active splitters
+    (count (filter #(= \^ %)
+                   ;; flatten the collection to make filtering easier
+                   (apply concat removed-inactive-splitters)))))
+
+(assert (= 21 (part1 sample-input)))
+
+(part1 full-input)
+;; => 1662
+
