d9c164ca47beb7276db90091399b38c471d50e94
Juraj Martinka <jumarko@gmail.com>
2025-12-08 14:00:05 +0100

    Advent of Code 2025 - day 6 - part2.

--- src/clojure_experiments/advent_of_code/advent_2025/day_06.clj
+++ src/clojure_experiments/advent_of_code/advent_2025/day_06.clj
@@ -1,6 +1,9 @@
 (ns clojure-experiments.advent-of-code.advent-2025.day-06
   "Cephalopod math homework.
   Input: https://adventofcode.com/2025/day/6/input.
+
+  Notice my program is quite long.
+  Compare to genmeblog: https://github.com/genmeblog/advent-of-code/blob/master/src/advent_of_code_2025/day06.clj
   "
   (:require
    [clojure-experiments.advent-of-code.utils :as utils]
@@ -35,8 +38,8 @@
          transpose)))
 
 
-(def sample-input (str/split-lines "123 328  51 64
- 45 64  387 23
+(def sample-input (str/split-lines "123 328  51 64 
+ 45 64  387 23 
   6 98  215 314
 *   +   *   +  "))
 
@@ -62,3 +65,116 @@
 
 (part1 full-parsed)
 ;; => 5060053676136
+
+
+;;; part2: reading numbers in columns, right to left
+
+;; e.g. (123 45 6) will become (356 24 1)
+
+(->> [123 45 6]
+     (map #(-> % str reverse) )
+     #_transpose)
+;; not quite right...
+;; => ((\3 \5 \6))
+
+(defn pad [avec n]
+  (let [length (count avec)]
+    (if (< length n)
+      (apply conj avec (repeat (- n length) nil))
+      avec))
+  )
+(pad [4 5] 3)
+;; => [4 5 nil]
+(pad [4 5 6] 3)
+;; => [4 5 6]
+
+(->> [123 45 6]
+     (map #(-> % str reverse vec (pad 3)) )
+     transpose
+     (map #(->> % (remove nil?) (apply str) parse-long))
+     )
+;; => (356 24 1)
+
+(defn- fix-line [[op & nums]]
+  (let [length (count (str (apply max-key #(-> % str count) nums)))
+        proper-nums (->> nums
+                         (map #(-> % str reverse vec (pad length)))
+                         transpose
+                         (map #(->> % (remove nil?) (apply str) parse-long)))]
+    (conj proper-nums op)))
+
+(fix-line (first sample-parsed))
+
+(defn part2
+  [parsed-input]
+  (part1 (mapv fix-line parsed-input)))
+
+(part2 sample-parsed)
+;; => 3264322
+;; BUT should be 3263827 ?
+
+(part2 full-parsed)
+;; => 9648030945893
+
+
+;;; let's try again...
+;;; I think, I need to find the longest number first,
+;;; then I need to align all the numbers in the same column properly.
+;;; Hmm... but the problem is that the alignment of numbers in the column
+;;; is really significant - so I cannot ignore whitespaces!
+;;; Therefore, it's too late to try to fix the parsed input  at this point.
+;;; => I need to re-do the parsing...
+
+(defn- parse-num [digits-seq]
+  (reduce (fn [acc digit] (+ (* 10 acc)
+                             (parse-long (str digit))))
+          0
+          digits-seq))
+(parse-num '(\4 \3 \1))
+;; => 431
+
+(defn- parse-nums [nums-lines]
+  (->> nums-lines
+       (map reverse)
+       ;; read the numbers in columns
+       transpose
+       (keep (fn [digits]
+               (filter #(not= \space %) digits)))
+       ;; group digits belonging to the same logical group (to which the operation in the last row will be applied)
+       ;; we know that each such group is followed by a column that has only spaces
+       (partition-by empty?)
+       ;; remove the spaces
+       (remove #(empty? (first %)))
+       ;; combine sequences of digits into numbers
+       (mapv #(map parse-num %))))
+(parse-nums (butlast sample-input))
+;; => [(4 431 623) (175 581 32) (8 248 369) (356 24 1)]
+
+(defn- parse-ops [op-line]
+  (->> (str/split op-line #"\s+")
+       (map (fn [op]
+              (case op
+                "*" *
+                "+" +)))
+       reverse))
+
+(defn parse-input2
+  "Parses the list of math problems into a sequence of forms such as
+  ['(* x y z) '(+ a b c) ...] where x, y, z, a, b, c, are numbers.
+  These forms should be directly evaluable."
+  [input-lines]
+  (let [nums (parse-nums (butlast input-lines))
+        ops (parse-ops (last input-lines))]
+    (mapv conj nums ops)))
+
+(parse-input2 sample-input)
+;; => [(#function[clojure.core/+] 4 431 623)
+;;     (#function[clojure.core/*] 175 581 32)
+;;     (#function[clojure.core/+] 8 248 369)
+;;    (#function[clojure.core/*] 356 24 1)]
+
+;; NOTE: we can reuse `part1` implementation for part2 here!
+(assert (= 3263827 (part1 (parse-input2 sample-input))))
+
+(assert (= 9695042567249 (part1 (parse-input2 full-input))))
+
